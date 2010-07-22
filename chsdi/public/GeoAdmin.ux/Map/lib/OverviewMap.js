9a5603bd4b72676d7f5bc8b17cc915f59ac34467
c2cfredj <c2cfredj@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2010-07-22 07:27:36 +0000

    coding style, fix image size
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@61 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/GeoAdmin.ux/Map/lib/OverviewMap.js
+++ chsdi/public/GeoAdmin.ux/Map/lib/OverviewMap.js
@@ -6,10 +6,16 @@
 GeoAdmin.OverviewMap = OpenLayers.Class(OpenLayers.Control.OverviewMap, {
 
     initialize: function(options) {
-        var layer = new OpenLayers.Layer.Image("overview",
-                                               OpenLayers.Util.getImagesLocation() + "keymap.png",
-                                               new OpenLayers.Bounds(485000, 65000, 835000, 298000),
-                                               new OpenLayers.Size(150, 99));
+        var url = OpenLayers.Util.getImagesLocation() + "keymap.png";
+        var extent = new OpenLayers.Bounds(485000, 65000, 835000, 298000);
+        var size = new OpenLayers.Size(150, 99);
+
+        var layer = new OpenLayers.Layer.Image("overview", url, extent, size, {
+            // fixed size
+            setTileSize: function() {
+                this.tileSize = this.size.clone();
+            }
+        });
         options = OpenLayers.Util.extend(options, {
             layers: [layer],
             size: new OpenLayers.Size(185, 105),
