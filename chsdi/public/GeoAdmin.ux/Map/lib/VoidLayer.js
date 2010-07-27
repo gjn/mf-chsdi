a9fd16c0d0db39e186b126bfe600e139b65ce3cf
c2cfredj <c2cfredj@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2010-07-27 13:18:55 +0000

    implementation update: the voidlayer changes the aerial opacity (needs more IE tests)
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@129 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/GeoAdmin.ux/Map/lib/VoidLayer.js
+++ chsdi/public/GeoAdmin.ux/Map/lib/VoidLayer.js
@@ -5,39 +5,30 @@
  */
 
 GeoAdmin.VoidLayer = OpenLayers.Class(OpenLayers.Layer, {
-    tile: null,
-    backgroundColor: "white",
+
+    aerial: null,
 
     initialize: function(name, options) {
         OpenLayers.Layer.prototype.initialize.apply(this, arguments);
-        this.tile = OpenLayers.Util.createDiv();
-        this.tile.style.backgroundColor = this.backgroundColor;
-        this.tile.style.opacity = this.opacity;
-
-        var inner = OpenLayers.Util.createDiv();
-        inner.appendChild(this.tile);
-        this.div.appendChild(inner);
     },
 
-    moveTo: function(bounds, zoomChanged, dragging) {
-        OpenLayers.Layer.prototype.moveTo.apply(this, arguments);
-        // size
-        var size = this.map.getSize();
-        this.tile.style.width = size.w + "px";
-        this.tile.style.height = size.h + "px";
+    destroy: function() {
+        // switch back the aerial opacity
+        this.aerial.setOpacity(1.0);
+        this.aerial = null;
 
-        // position
-        this.tile.style.top = this.inverse(this.map.layerContainerDiv.style.top);
-        this.tile.style.left = this.inverse(this.map.layerContainerDiv.style.left);
+        OpenLayers.Layer.prototype.destroy.apply(this, arguments);
     },
 
-    inverse: function(position) {
-        if (position.charAt(0) === '-') {
-            // remove minus sign
-            return position.substr(1);
-        } else {
-            // addd minus sign
-            return "-" + position;
-        }
+    setMap: function(map) {
+        OpenLayers.Layer.prototype.setMap.apply(this, arguments);
+        this.aerial = map.aerial;
+        //this.aerial.setVisibility(true);
+    },
+
+    setOpacity: function(opacity) {
+        this.opacity = opacity;
+        this.aerial.setVisibility(this.opacity > 0.0);
+        this.aerial.setOpacity(this.opacity);
     }
 });
