982367b49b9164a42aa6cdf5c1059da569cba6d5
c2cfredj <c2cfredj@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2010-07-29 13:06:19 +0000

    add Babel
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@148 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author_email = '',
     url = '',
     install_requires = [
-        "mapfish", "JSTools", "Sphinx"
+        "mapfish", "JSTools", "Sphinx", "Babel"
     ],
     #setup_requires=["PasteScript>=1.6.3"],
     packages = find_packages(exclude=['ez_setup']),
