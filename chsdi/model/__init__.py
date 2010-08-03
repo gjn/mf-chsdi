c621e7b6e77eb32ecf59d54ace8fefa06517a4d8
c2cfredj <c2cfredj@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2010-08-03 08:43:04 +0000

    more code from geoadmin
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@181 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/model/__init__.py
+++ chsdi/model/__init__.py
@@ -1,27 +1,56 @@
-"""The application's model objects"""
-import sqlalchemy as sa
-from sqlalchemy import orm
+from sys import maxint
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy.types import Integer, Text, String
+
+from mapfish.lib.filters.spatial import Spatial
+
+from geojson.feature import Feature
+from geoalchemy import Geometry
+from shapely.wkb import loads
 
 from chsdi.model import meta
 
 def init_model(key, engine):
-    """Call me before using any of the tables or classes in the model"""
     if key not in meta.engines:
         meta.engines[key] = engine
+
+def register(name, klass):
+    name = unicode(name)
+    if name not in meta.bodmap:
+        meta.bodmap[name] = []
+    meta.bodmap[name].append(klass)
+
+
+def models_from_name(name):
+    if name in meta.bodmap:
+        return meta.bodmap[name]
     else:
-        # FIXME: report error
-        pass
-        
+        return []
 
+class Queryable(object):
+    __minscale__ = 0
+    __maxscale__ = maxint
 
-    
-    ## Reflected tables must be defined and mapped here
-    #global reflected_table
-    #reflected_table = sa.Table("Reflected", meta.metadata, autoload=True,
-    #                           autoload_with=engine)
-    #orm.mapper(Reflected, reflected_table)
+    @classmethod
+    def bbox_filter(cls, scale, bbox):
+        if scale is None or scale in xrange(cls.__minscale__, cls.__maxscale__):
+            return Spatial(Spatial.BOX, cls.__table__.columns['the_geom'], box=bbox, tolerance=0, epsg=21781)
 
-#     sm = orm.sessionmaker(autoflush=True, autocommit=False, bind=engine)
+#         if scale is None or scale in xrange(cls.__minscale__, cls.__maxscale__):
+#             return Spatial(Spatial.BOX, cls.__table__.columns['the_geom'],
+#                            bbox=bbox, tolerance=0)
+    @property
+    def html(self):
+        c.feature = self
+        c.layer = self.bodlayer
+        return render(self.__template__)
 
-#     meta.engine = engine
-#     meta.Session = orm.scoped_session(sm)
+    @property
+    def geometry(self):
+        return loads(self.the_geom.geom_wkb.decode('hex'))
+
+    @property
+    def __geo_interface__(self):
+        return Feature(id=self.id, geometry=self.geometry)
