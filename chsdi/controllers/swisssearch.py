2d471eb0d19e50e2fd7be576652324919020d0b8
c2cfredj <c2cfredj@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2010-08-06 10:15:31 +0000

    add geocoding function
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@211 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/controllers/swisssearch.py
+++ chsdi/controllers/swisssearch.py
@@ -1,6 +1,7 @@
 import logging
 from operator import itemgetter
 
+from sqlalchemy import or_
 from sqlalchemy.sql import func
 
 from pylons import request, response
@@ -26,3 +27,19 @@ class SwisssearchController(BaseController):
         query = query.order_by(SwissSearch.search_name).limit(50)
 
         return {'results': sorted([f.json for f in query], key=itemgetter('rank'))}
+
+    @jsonify(cb="cb")
+    def geocoding(self):
+        lon = float(request.params.get('easting'))
+        lat = float(request.params.get('northing'))
+
+        # search for everything except sn25 data (who did not have 'the_geom_poly' geom)
+        gfilter_poly = SwissSearch.within_filter(lon, lat, tolerance=10, column='the_geom_poly')
+
+        # now search for sn25 data
+        gfilter_point = SwissSearch.within_filter(lon, lat, tolerance=200, column='the_geom_point')
+
+        query = Session.query(SwissSearch)
+        query = query.filter(or_(gfilter_poly.to_sql_expr(), gfilter_point.to_sql_expr()))
+
+        return [f.json for f in query.all()]
