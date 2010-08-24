dcab33516518a8eb4cd8cbbfc589c82ef0a8d727
c2cfredj <c2cfredj@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2010-08-24 13:25:00 +0000

    bod model
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@258 e100092c-b5e6-494f-8bdf-19c1ff2a4001

---
+++ chsdi/model/bod.py
@@ -0,0 +1,64 @@
+import os
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from pylons import config
+
+from chsdi.lib.base import render, c
+from chsdi.model import *
+
+Base = declarative_base(bind=meta.engines['bod'])
+
+class LayerLegend(Base):
+    __tablename__ = 'bod_layer_legende'
+    __table_args__ = ({'autoload': True})
+
+class BodLayer(object):
+    def json(self, hilight):
+        c.hilight = hilight
+        c.layer = self
+        # fixme
+        c.full = False
+
+        return {
+            'id': self.bod_layer_id,
+            'label': self.bezeichnung.strip(),
+            'datenherr': self.datenherr,
+            'content': render('/bod-details.mako')
+        }
+
+    @property
+    def short_abstract(self):
+        if self.abstract:
+            if len(self.abstract) > 230:
+                return self.abstract[:230] + '...'
+            else:
+                return self.abstract
+        else:
+            return ''
+
+    @property
+    def inspire_themes(self):
+        if self.inspire_oberthema_name is not None:
+            names = [self.inspire_oberthema_name+" > ", self.inspire_name]
+        else:
+            names = [self.inspire_name]
+
+        return " ".join([n for n in names if n is not None])
+
+    def legend(self, lang):
+        # FIXME: use get_lang
+        fpath = str("gfx/legend/%(id)s_%(lang)s.png"%{'id': self.bod_layer_id, 'lang': lang})
+        root = config['pylons.paths']['static_files']
+        if os.path.exists(os.path.join(root, fpath)):
+            return h.url_for('/' + fpath)
+        else:
+            return False
+
+class BodLayerDe(Base, BodLayer):
+    __tablename__ = 'bod_layer_suche_de'
+    __table_args__ = ({'autoload': True})
+
+class BodLayerFr(Base, BodLayer):
+    __tablename__ = 'bod_layer_suche_fr'
+    __table_args__ = ({'autoload': True})
