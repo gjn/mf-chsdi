e1e842cffdc3f67ca34f6f396eaf5516d20fd35c
c2cfredj <c2cfredj@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2010-08-24 13:20:42 +0000

    entry controller was removed
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@257 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/tests/functional/test_entry.py
+++
@@ -1,7 +0,0 @@
-from chsdi.tests import *
-
-class TestEntryController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='entry', action='index'))
-        # Test response...
