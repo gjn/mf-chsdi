4d7f942df6e6ec13dc029aea3f949e82be85b24a
c2cfredj <c2cfredj@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2010-08-24 12:43:53 +0000

    bodsearch controller
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@253 e100092c-b5e6-494f-8bdf-19c1ff2a4001

---
+++ chsdi/controllers/bodsearch.py
@@ -0,0 +1,48 @@
+import logging
+
+from pylons import request, response, tmpl_context as c
+from pylons.controllers.util import abort
+
+from chsdi.lib.base import BaseController, render, jsonify
+from chsdi.model.bod import BodLayerDe, BodLayerFr, LayerLegend
+from chsdi.model.meta import Session
+
+log = logging.getLogger(__name__)
+
+class BodsearchController(BaseController):
+
+    def __before__(self):
+        super(BodsearchController, self).__before__()
+        if self.lang == 'fr':
+            self.BodLayer = BodLayerFr
+        else:
+            self.BodLayer = BodLayerDe
+
+    @jsonify(cb='cb')
+    def search(self):
+        q = request.params.get('query')
+        if q is None:
+            abort(400, "missing 'query' parameter")
+
+        query = Session.query(self.BodLayer)
+        query = query.filter(self.BodLayer.volltextsuche.ilike('%' + q + '%'))
+
+        return {'results': [r.json(q) for r in query]}
+
+    def details(self, id=None):
+        c.full = True
+        c.hilight = ''
+        c.layer = Session.query(self.BodLayer).get(id)
+        if c.layer is None:
+            abort(404)
+
+        c.legend = Session.query(LayerLegend).get(id)
+        if c.legend is None:
+            abort(404)
+
+        if 'print' in request.params:
+            #baseUrl = request.params.get('baseUrl')
+            return render('/bod-details-print.mako')
+        else:
+            return render('/bod-details.mako')
+
