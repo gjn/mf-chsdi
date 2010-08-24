b9465b6b9861afeffd5ad8422db566c620d07408
c2cfredj <c2cfredj@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2010-08-24 12:43:08 +0000

    finish hilight helper function
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@252 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/lib/helpers.py
+++ chsdi/lib/helpers.py
@@ -3,5 +3,13 @@
 Consists of functions to typically be used within templates, but also
 available to Controllers. This module is available to templates as 'h'.
 """
-# Import helpers as desired, or define your own, ie:
-#from webhelpers.html.tags import checkbox, password
+
+def hilight(string, search, prefix="<span class='match'>", postfix="</span>"):
+    if string is not None and search != '':
+        start = string.lower().find(search.lower())
+        while start > -1:
+            stop = start + len(search.lower())
+            string = string[:start] + prefix + string[start:stop] + postfix + string[stop:]
+            next = len(string[:start] + prefix + string[start:stop] + postfix)
+            start = string.lower().find(search.lower(), next)
+    return string
