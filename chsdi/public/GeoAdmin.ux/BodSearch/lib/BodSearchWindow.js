dcf07a71edeec88f9e2671207aeeb417c5910fb0
c2cfredj <c2cfredj@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2010-08-30 14:33:07 +0000

    display layer info
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@306 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/GeoAdmin.ux/BodSearch/lib/BodSearchWindow.js
+++ chsdi/public/GeoAdmin.ux/BodSearch/lib/BodSearchWindow.js
@@ -1,5 +1,36 @@
+/*
+ * @include Ext/src/ext-core/examples/jsonp/jsonp.js
+ */
+
 GeoAdmin.BodSearchWindow = {
+
     show: function(id) {
-        alert(id);
+        var _window = new Ext.Window({
+            layout: 'anchor',
+//            closeAction: 'hide',
+            resizable: true,
+            autoScroll: true,
+            width: 525,
+            items: {html: ''}
+        });
+
+        _window._url = GeoAdmin.webServicesUrl + '/bodsearch/details/' + id;
+
+        Ext.ux.JSONP.request(_window._url, {
+            callbackKey: "cb",
+            params: {
+                lang: OpenLayers.Lang.getCode()
+            },
+            scope: _window,
+            callback: function(response) {
+                this.removeAll();
+                this.add({
+                    html: response,
+                    cls: 'bodsearch'
+                });
+                this.doLayout();
+                this.show();
+            }
+        });
     }
 };
