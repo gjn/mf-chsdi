1fafb335925bb184e3babdef5be1f902a7301bd4
c2cfredj <c2cfredj@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2010-09-01 13:18:16 +0000

    Tooltip control (not finished yet)
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@321 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/GeoAdmin.ux/Features/lib/Tooltip.js
+++ chsdi/public/GeoAdmin.ux/Features/lib/Tooltip.js
@@ -1,10 +1,11 @@
 /*
+ * @include Ext/src/ext-core/examples/jsonp/jsonp.js
+ *
  * @requires OpenLayers/Control/GetFeature.js
- * @include OpenLayers/Protocol/HTTP.js
  * @include OpenLayers/Format/GeoJSON.js
- * @include OpenLayers/Lang.js
+ *
+ * @include GeoExt/widgets/Popup.js
  */
-// use http://trac.geoext.org/browser/sandbox/camptocamp/extensions/geoext.ux/ux/FeatureBrowser to display results
 GeoAdmin.Tooltip = OpenLayers.Class(OpenLayers.Control.GetFeature, {
 
     /*
@@ -18,11 +19,12 @@ GeoAdmin.Tooltip = OpenLayers.Class(OpenLayers.Control.GetFeature, {
     url: null,
 
     initialize: function(options) {
+        OpenLayers.Control.GetFeature.prototype.initialize.apply(this, arguments);
+
         if (GeoAdmin.webServicesUrl) {
             this.url = GeoAdmin.webServicesUrl + "/bodfeature/search";
         }
-        OpenLayers.Control.GetFeature.prototype.initialize.apply(this, arguments);
-
+        this.format = new OpenLayers.Format.GeoJSON();
         this.protocol = new OpenLayers.Protocol.HTTP({
             url: url,
             format: new OpenLayers.Format.GeoJSON(),
@@ -31,15 +33,13 @@ GeoAdmin.Tooltip = OpenLayers.Class(OpenLayers.Control.GetFeature, {
             }
         });
 
-//         this.events.on({
-//             "featuresselected": this.onSelect,
-//             "featureunselected":this.onUnSelect,
-//             scope: this
-//         });
+        this.events.register("featuresselected", this, this.onSelect);
+        this.events.register("featureunselected", this, this.onUnselect);
     },
 
     setMap: function(map) {
         OpenLayers.Control.GetFeature.prototype.setMap.apply(this, arguments);
+
         this.map.events.on({
             addlayer: this.updateLayersList,
             removelayer: this.updateLayersList,
@@ -50,23 +50,74 @@ GeoAdmin.Tooltip = OpenLayers.Class(OpenLayers.Control.GetFeature, {
 
     updateLayersList: function(layer) {
         this.queryable = [];
-        var layers = this.map.getLayersBy('geoadmin_queryable', true);
+        var layers = this.map.getLayersBy("geoadmin_queryable", true);
         for (var i = 0, len = layers.length; i < len; i++) {
-            // FIXME: get the name ? not the layername ?
-            this.queryable.push(layers[i].name);
+            this.queryable.push(layers[i].layername);
         }
     },
 
     request: function(bounds, options) {
         if (this.queryable.length > 0) {
-            this.protocol.params.layers = this.queryable;
-            OpenLayers.Control.GetFeature.prototype.request.apply(this, arguments);
+            // Set the cursor to "wait" to tell the user we're working.
+            OpenLayers.Element.addClass(this.map.viewPortDiv, "olCursorWait");
+
+            Ext.ux.JSONP.request(this.url, {
+                callbackKey: "cb",
+                params: {
+                    lang: OpenLayers.Lang.getCode(),
+                    layers: this.queryable.join(","),
+                    bbox: bounds.toBBOX()
+                },
+                scope: this,
+                callback: function(response) {
+                    var features = this.format.read(response);
+                    this.unselectAll();
+                    if (features.length > 0) {
+                        this.select(features);
+                    }
+                    // reset the cursor.
+                    OpenLayers.Element.removeClass(this.map.viewPortDiv, "olCursorWait");
+                }
+            });
         }
     },
 
+    activate: function() {
+        if (!this.layer) {
+            this.layer = new OpenLayers.Layer.Vector();
+            this.map.addLayer(this.layer);
+        }
+        if (!this.popup) {
+            this.popup = new GeoExt.Popup({
+                map: this.map
+            });
+        }
+        return OpenLayers.Control.GetFeature.prototype.activate.apply(this, arguments);
+    },
+
+    deactivate: function() {
+        if (this.layer) {
+            this.layer.destroy();
+            this.layer = null;
+        }
+        
+        if (this.popup) {
+            this.popup.destroy();
+            this.popup = null;
+        }
+        return OpenLayers.Control.GetFeature.prototype.deactivate.apply(this, arguments);
+    },
+
+    destroy: function() {
+        this.deactivate();
+        OpenLayers.Control.GetFeature.prototype.destroy.apply(this, arguments);
+    },
+
     onSelect: function(evt) {
+        this.layer.addFeatures(evt.features);
     },
 
     onUnselect: function(evt) {
+        this.layer.removeFeatures([evt.feature]);
     }
 });
