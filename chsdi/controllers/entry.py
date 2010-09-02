09915b2942343c45566f7feeb348438caef53a38
c2cfredj <c2cfredj@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2010-09-02 10:24:26 +0000

    change INSTANCE_ID
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@330 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/controllers/entry.py
+++ chsdi/controllers/entry.py
@@ -1,6 +1,6 @@
 import logging
 
-from pylons import request, response, session, tmpl_context as c
+from pylons import request, response, tmpl_context as c
 from pylons.controllers.util import abort
 
 from chsdi.lib.base import BaseController, render
@@ -11,6 +11,7 @@ class EntryController(BaseController):
 
     def loader(self):
         c.wsgi_base = request.environ["SCRIPT_NAME"]
+        #response.headers['Content-Type'] = 'text/javascript'
         return render("loader.js")
 
     def do_not_exist(self):
