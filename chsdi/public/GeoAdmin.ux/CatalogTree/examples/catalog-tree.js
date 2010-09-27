72a19222f6a92f74b06eea89f7d1ead34e2d6e90
c2celemoine <c2celemoine@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2010-09-27 13:12:48 +0000

    add a map to the catalog-tree example
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@362 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/GeoAdmin.ux/CatalogTree/examples/catalog-tree.js
+++ chsdi/public/GeoAdmin.ux/CatalogTree/examples/catalog-tree.js
@@ -1,7 +1,10 @@
 Ext.onReady(function() {
+    
+    // create a map, and center it to its maximum extent
+    // (map is made global for easy debugging)
+    map = new GeoAdmin.Map("map");
+    map.zoomToMaxExtent();
 
-    new GeoAdmin.CatalogTree({
-        renderTo: "catalog-tree"
-    });
-
+    // create a catalog tree bound to this map
+    new GeoAdmin.CatalogTree({renderTo: "catalog-tree", map: map});
 });
