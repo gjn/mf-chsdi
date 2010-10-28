a92a4ffb74646e309025277dceb2020a83547b2c
c2celemoine <c2celemoine@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2010-10-28 16:14:22 +0000

    use two permalink panels in the example, to demonstrate that toggle works
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@546 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/GeoAdmin.ux/Permalink/examples/permalink-panel.js
+++ chsdi/public/GeoAdmin.ux/Permalink/examples/permalink-panel.js
@@ -1,6 +1,6 @@
 /*global Ext, OpenLayers, GeoExt, GeoAdmin*/ 
 
-var mainPanel, mapPanel, permalinkPanel;
+var mainPanel, mapPanel, permalinkPanel1, permalinkPanel2;
 
 Ext.onReady(function() {
 
@@ -9,7 +9,8 @@ Ext.onReady(function() {
     Ext.state.Manager.setProvider(
         new GeoExt.state.PermalinkProvider({encodeType: false}));
 
-    permalinkPanel = new GeoAdmin.PermalinkPanel();
+    permalinkPanel1 = new GeoAdmin.PermalinkPanel();
+    permalinkPanel2 = new GeoAdmin.PermalinkPanel();
 
     mapPanel = new GeoExt.MapPanel({
         region: "center",
@@ -17,13 +18,25 @@ Ext.onReady(function() {
         width: 600,
         map: new GeoAdmin.Map(),
         stateId: "map",
-        tbar: [{
-            text: "permalink",
-            handler: function() {
-                permalinkPanel.setVisible(!permalinkPanel.isVisible());
-            }
+        tbar: ["->", {
+            text: "permalink 1",
+            enableToggle: true,
+            toggleGroup: "export",
+            allowDepress: true,
+            toggleHandler: function(btn, state) {
+                permalinkPanel1.setVisible(state);
+            },
+        }, {
+            text: "permalink 2",
+            enableToggle: true,
+            toggleGroup: "export",
+            allowDepress: true,
+            toggleHandler: function(btn, state) {
+                permalinkPanel2.setVisible(state);
+            },
+            toggleGroup: "export"
         }],
-        items: permalinkPanel
+        items: [permalinkPanel1, permalinkPanel2]
     });
 
     mainPanel = new Ext.Panel({
