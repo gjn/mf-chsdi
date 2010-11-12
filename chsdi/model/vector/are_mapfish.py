1834c5f9452fd3c3f0e3418a639d107e3c365520
ltfec <ltfec@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2010-11-12 12:53:40 +0000

    more are layer
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@716 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/model/vector/are_mapfish.py
+++ chsdi/model/vector/are_mapfish.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from chsdi.model import *
 
 Base = declarative_base(bind=meta.engines['are_mapfish'])
@@ -15,3 +16,36 @@ class PersonenStrasse(Base, Queryable):
     the_geom = Column(Geometry)
 
 register('ch.are.belastung-personenverkehr-strasse-2008', PersonenStrasse)
+
+
+class GueterStrasse(Base, Queryable):
+    # view in a schema
+    __tablename__ = 'belastung_gueterverkehr_strasse_2008'
+    __table_args__ = ({'schema': 'strassen', 'autoload': True})
+    __template__ = 'tooltips/gueterstrasse.mako'
+
+   # __minscale__ = 5001
+   # __maxscale__ = 25000
+
+    id = Column('row_id', Integer, primary_key=True)
+    the_geom = Column(Geometry)
+
+register('ch.are.belastung-gueterverkehr-strasse-2008', GueterStrasse)
+
+
+# class PersonenBahn
+
+
+class GueterBahn(Base, Queryable):
+    # view in a schema
+    __tablename__ = 'belastung_gueterverkehr_bahn_2008'
+    __table_args__ = ({'schema': 'oeffentlicher_verkehr', 'autoload': True})
+    __template__ = 'tooltips/gueterbahn.mako'
+
+   # __minscale__ = 5001
+   # __maxscale__ = 25000
+
+    id = Column('row_id', Integer, primary_key=True)
+    the_geom = Column(Geometry)
+
+register('ch.are.belastung-gueterverkehr-bahn-2008', GueterBahn)
