d3c1f0e6ed79d2cdb18e84d4f7848283d9d23e9b
ltfec <ltfec@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2010-11-29 15:00:48 +0000

    more classes for are tooltips
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@777 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/model/vector/are_mapfish.py
+++ chsdi/model/vector/are_mapfish.py
@@ -9,9 +9,6 @@ class PersonenStrasse(Base, Queryable):
     __table_args__ = ({'schema': 'strassen', 'autoload': True})
     __template__ = 'tooltips/personenstrasse.mako'
 
-   # __minscale__ = 5001
-   # __maxscale__ = 25000
-
     id = Column('row_id', Integer, primary_key=True)
     the_geom = Column(Geometry)
 
@@ -24,16 +21,22 @@ class GueterStrasse(Base, Queryable):
     __table_args__ = ({'schema': 'strassen', 'autoload': True})
     __template__ = 'tooltips/gueterstrasse.mako'
 
-   # __minscale__ = 5001
-   # __maxscale__ = 25000
-
     id = Column('row_id', Integer, primary_key=True)
     the_geom = Column(Geometry)
 
 register('ch.are.belastung-gueterverkehr-strasse-2008', GueterStrasse)
 
 
-# class PersonenBahn
+class PersonenBahn(Base, Queryable):
+    # view in a schema
+    __tablename__ = 'belastung_personenverkehr_bahn_2008'
+    __table_args__ = ({'schema': 'oeffentlicher_verkehr', 'autoload': True})
+    __template__ = 'tooltips/personenbahn.mako'
+
+    id = Column('row_id', Integer, primary_key=True)
+    the_geom = Column(Geometry)
+
+register('ch.are.belastung-personenverkehr-bahn-2008', PersonenBahn)
 
 
 class GueterBahn(Base, Queryable):
@@ -42,10 +45,43 @@ class GueterBahn(Base, Queryable):
     __table_args__ = ({'schema': 'oeffentlicher_verkehr', 'autoload': True})
     __template__ = 'tooltips/gueterbahn.mako'
 
-   # __minscale__ = 5001
-   # __maxscale__ = 25000
-
     id = Column('row_id', Integer, primary_key=True)
     the_geom = Column(Geometry)
 
 register('ch.are.belastung-gueterverkehr-bahn-2008', GueterBahn)
+
+
+class Landschaftstypen(Base, Queryable):
+    # view in a schema
+    __tablename__ = 'landschaftstypen'
+    __table_args__ = ({'schema': 'siedlung_landschaft', 'autoload': True})
+    __template__ = 'tooltips/landschaftstypen.mako'
+
+    id = Column('row_id', Integer, primary_key=True)
+    the_geom = Column(Geometry)
+
+register('ch.are.landschaftstypen', Landschaftstypen)
+
+
+class Alpenkonvention(Base, Queryable):
+    # view in a schema
+    __tablename__ = 'alpenkonvention'
+    __table_args__ = ({'schema': 'siedlung_landschaft', 'autoload': True})
+    __template__ = 'tooltips/alpenkonvention.mako'
+
+    id = Column('row_id', Integer, primary_key=True)
+    the_geom = Column(Geometry)
+
+register('ch.are.alpenkonvention', Alpenkonvention)
+
+
+class AggloIsoStaedte(Base, Queryable):
+    # view in a schema
+    __tablename__ = 'agglomerationen_isolierte_staedte_2000'
+    __table_args__ = ({'schema': 'siedlung_landschaft', 'autoload': True})
+    __template__ = 'tooltips/aggloisostaedte.mako'
+
+    id = Column('row_id', Integer, primary_key=True)
+    the_geom = Column(Geometry)
+
+register('ch.are.agglomerationen_isolierte_staedte-2000', AggloIsoStaedte)
