8b0f173e1070488bb28c0491e0aba2654c6f2733
ltmom <ltmom@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2010-12-08 14:08:34 +0000

    add navigation history api doc
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@830 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/GeoAdmin.ux/NavigationHistory/lib/NavigationHistory.js
+++ chsdi/public/GeoAdmin.ux/NavigationHistory/lib/NavigationHistory.js
@@ -6,6 +6,44 @@
  * @include GeoExt/widgets/Action.js
  */
 
+/** api: (define)
+ *  module = GeoAdmin
+ *  class = NavigationHistory
+ *  base_link = `Ext.Container <http://dev.sencha.com/deploy/dev/docs/?class=Ext.Container>`_
+ */
+
+
+/** api: example
+ *  Sample code to create a map panel with a navigation history control (see also :ref:`navigation-history`):
+ *
+ *  .. code-block:: javascript
+ *
+ *     map = new GeoAdmin.Map();
+ *     mapPanel = new GeoExt.MapPanel({
+ *         title: "MapPanel",
+ *          renderTo: "map",
+ *          map: map,
+ *          height: 400,
+ *          width: 600,
+ *          bbar: [
+ *              new GeoAdmin.NavigationHistory({
+ *                  map: map
+ *               })
+ *          ]
+ *     });
+ *
+ */
+
+/** api: constructor
+ *  .. class:: NavigationHistory(config)
+ *
+ *  :param config: ``Object`` config
+ *
+ *  :return:  ``GeoAdmin.NavigationHistory``
+ *
+ *  Create a navigation history control with two button to navigate forward and backward in history 
+ */
+
 GeoAdmin.NavigationHistory = Ext.extend(Ext.Container, {
 
     map: null,
