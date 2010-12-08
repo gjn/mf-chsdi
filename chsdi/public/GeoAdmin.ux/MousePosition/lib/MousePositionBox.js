6bf3ac97ba53930c68944eb92fc7eceb461e7979
ltmom <ltmom@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2010-12-08 13:50:43 +0000

    mouseposition api doc and example
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@829 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/GeoAdmin.ux/MousePosition/lib/MousePositionBox.js
+++ chsdi/public/GeoAdmin.ux/MousePosition/lib/MousePositionBox.js
@@ -4,6 +4,44 @@
  * @include OpenLayers/Control/MousePosition.js
  */
 
+/** api: (define)
+ *  module = GeoAdmin
+ *  class = MousePositionBox
+ *  base_link = `Ext.BoxComponent <http://dev.sencha.com/deploy/dev/docs/?class=Ext.BoxComponent>`_
+ */
+
+
+/** api: example
+ *  Sample code to create a map panel with a mouse position control (see also :ref:`mouse-position`):
+ *
+ *  .. code-block:: javascript
+ *
+ *     map = new GeoAdmin.Map();
+ *     mapPanel = new GeoExt.MapPanel({
+ *         title: "MapPanel",
+ *          renderTo: "map",
+ *          map: map,
+ *          height: 400,
+ *          width: 600,
+ *          bbar: [
+ *              new GeoAdmin.MousePositionBox({
+ *                  map: map
+ *               })
+ *          ]
+ *     });
+ *
+ */
+
+/** api: constructor
+ *  .. class:: MousePosition(config)
+ *
+ *  :param config: ``Object`` config
+ *
+ *  :return:  ``GeoAdmin.MousePosition``
+ *
+ *  Create a control to display the mouse position 
+ */
+
 GeoAdmin.MousePositionBox = Ext.extend(Ext.BoxComponent, {
 
     afterRender: function() {
