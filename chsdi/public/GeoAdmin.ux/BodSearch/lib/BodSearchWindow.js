f95b15a54539b34bf3477a4a603a1fb884b2b9ed
ltmoc <ltmoc@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2011-01-20 08:53:55 +0000

    Add infobox in metadata window. image is wrong. need to be transparent. Will be done by Ambrogio
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@942 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/GeoAdmin.ux/BodSearch/lib/BodSearchWindow.js
+++ chsdi/public/GeoAdmin.ux/BodSearch/lib/BodSearchWindow.js
@@ -19,16 +19,16 @@
 
 GeoAdmin.BodSearchWindow = {
 
-   /** api: method[show]
-    *  :param id: ``String``: the layer id. The layer list can be found `here <http://api.geo.admin.ch/main/wsgi/doc/build/api/faq/index.html#which-layers-are-available>`_
-    *
-    *  Show a layer metadata window
-    */
+    /** api: method[show]
+     *  :param id: ``String``: the layer id. The layer list can be found `here <http://api.geo.admin.ch/main/wsgi/doc/build/api/faq/index.html#which-layers-are-available>`_
+     *
+     *  Show a layer metadata window
+     */
     show: function(id) {
 
         var _window = new Ext.Window({
             layout: 'anchor',
-            title: OpenLayers.i18n('infobox'),
+            closeAction: 'hide',
             resizable: true,
             autoScroll: true,
             border: false,
@@ -36,28 +36,48 @@ GeoAdmin.BodSearchWindow = {
             bodyStyle: "max-height: 500px",
             items: {html: ''},
             toolTemplate: new Ext.XTemplate(
-                '<tpl if="id==\'print\'">',
-                '<div class="x-window-printtool">'+OpenLayers.i18n('print')+'</div>',
-                '</tpl>',
-                '<tpl if="id!=\'print\'">',
-                '<div class="x-tool x-tool-{id}">&#160;</div>',
-                '</tpl>'
-            ),
-            tools: [{
-                id: 'print',
-                handler: function(evt, toolEl, panel, tc) {
-                    // remove the callbackKey introduced by
-                    // Ext.ux.JSONP.request and add the print key.
-                    delete panel._params['cb'];
-                    panel._params['print'] = true;
-                    var url = Ext.urlAppend(panel._url, Ext.urlEncode(panel._params));
-                    window.open(url, '', 'width=500, height=400, toolbar=no, location=no,' +
-                                         'directories=no, status=no, menubar=no, scrollbars=yes,' +
-                                         'copyhistory=no, resizable=no');
+                    '<tpl if="id==\'infoBox\'">',
+                    '<div class="x-tool x-tool-infoBox">' + OpenLayers.i18n('infobox') + '</div>',
+                    '</tpl>',
+                    '<tpl if="id==\'infoTool\'">',
+                    '<div class="x-tool x-tool-infoTool">&#160;</div>',
+                    '</tpl>',
+                    '<tpl if="id==\'pipe4\'">',
+                    '<div class="x-tool x-tool-pipe4">&#160;</div>',
+                    '</tpl>',
+                    '<tpl if="id==\'print\'">',
+                    '<div class="x-window-printtool">' + OpenLayers.i18n('print') + '</div>',
+                    '</tpl>',
+                    '<tpl if="id==\'close\'">',
+                    '<div class="x-tool x-tool-close">&#160;</div>',
+                    '</tpl>'
+                    ),
+            tools: [
+                {
+                    id: 'infoBox'
+                },
+                {
+                    id: 'infoTool'
+                },
+                {
+                    id: 'pipe4'
+                },
+                {
+                    id: 'print',
+                    handler: function(evt, toolEl, panel, tc) {
+                        // remove the callbackKey introduced by
+                        // Ext.ux.JSONP.request and add the print key.
+                        delete panel._params['cb'];
+                        panel._params['print'] = true;
+                        var url = Ext.urlAppend(panel._url, Ext.urlEncode(panel._params));
+                        window.open(url, '', 'width=500, height=400, toolbar=no, location=no,' +
+                                'directories=no, status=no, menubar=no, scrollbars=yes,' +
+                                'copyhistory=no, resizable=no');
+                    }
                 }
-            }]
+            ]
         });
-        
+
         _window._url = GeoAdmin.webServicesUrl + '/bodsearch/details/' + id;
 
         _window._params = {
