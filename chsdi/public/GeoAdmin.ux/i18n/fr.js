14d076f93a98e37c8dcf839d6646c01d76cb9d62
ltfoa <ltfoa@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2011-02-15 13:28:56 +0000

    translation typo (refs #2245)
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@1011 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/GeoAdmin.ux/i18n/fr.js
+++ chsdi/public/GeoAdmin.ux/i18n/fr.js
@@ -186,7 +186,7 @@ OpenLayers.Util.extend(OpenLayers.Lang.fr, {
     'ch.swisstopo.geologie-hydrogeologische_karte-grundwasservorkommen': 'Réservoirs aquifères',
     'ch.swisstopo.geologie-hydrogeologische_karte-grundwasservulnerabilitaet': 'Vulnérabilité des aquifères',
     'ch.swisstopo.geologie-tektonische_karte': 'Tectonique',
-    'ch.are.agglomerationen_isolierte_staedte-2000': 'Agglomérations et villes isolées 2000',
+    'ch.are.agglomerationen_isolierte_staedte-2000': 'Agglomérations et villes isolées 2',
     'ch.are.alpenkonvention': 'Convention des Alpes',
     'ch.are.gemeindetyp-1990-9klassen': 'Typologie des communes ARE',
     'ch.astra.ivs-nat': 'IVS national',
