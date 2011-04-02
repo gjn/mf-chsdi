7663d6900371dc6b0619ae0b37b1d7afee3632ea
ltmoc <ltmoc@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2011-04-02 17:40:10 +0000

    refactoring and optimizations
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@1099 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/demo/mobile.js
+++ chsdi/public/demo/mobile.js
@@ -2,7 +2,7 @@
 var map;
 var pixelmap;
 var wmts;
-var sm =  new OpenLayers.Projection("EPSG:21781");
+var sm = new OpenLayers.Projection("EPSG:21781");
 var vector = new OpenLayers.Layer.Vector('vector');
 var initial_zoom_level = 0;
 var LAYER_NAME = 'ch.bafu.schutzgebiete-wildruhezonen';
@@ -14,56 +14,56 @@ for (var m in matrixDefs) {
     var matrix = {};
     matrix.identifier = m;
     matrix.scaleDenominator = def.scaleDenominator;
-    matrix.topLeftCorner =  new OpenLayers.LonLat(def.topLeftCorner[0], def.topLeftCorner[1]);
-    matrixIds.push(matrix);          
-};
+    matrix.topLeftCorner = new OpenLayers.LonLat(def.topLeftCorner[0], def.topLeftCorner[1]);
+    matrixIds.push(matrix);
+}
 
 function checkIsInLayer(bounds) {
-        
-         Ext.util.JSONP.request({
-                url: 'http://api.geo.admin.ch/feature/search',
-                callbackKey: 'cb',
-                params: {
-                    bbox: bounds.toBBOX(),
-                    layers: LAYER_NAME,
-                    cb: 'Ext.ux.JSONP.callback',
-                    format: 'raw',
-                    no_geom: true
-                },
-                callback: function(data) {
-                    if(data.features.length > 0) {
-                              var warning = Ext.getCmp('warning');
-                              warning.title = "In a protected area";
-                              warning.ui = 'dark';
-                    } else {
-                              var warning = Ext.getCmp('warning');
-                              warning.title = "Not in a protected area";
-                              warning.ui = 'light';
-                    }
-                }
-            });
-   
- };
- 
+
+    Ext.util.JSONP.request({
+        url: 'http://api.geo.admin.ch/feature/search',
+        callbackKey: 'cb',
+        params: {
+            bbox: bounds.toBBOX(),
+            layers: LAYER_NAME,
+            cb: 'Ext.ux.JSONP.callback',
+            format: 'raw',
+            no_geom: true
+        },
+        callback: function(data) {
+            var warning;
+            if (data.features.length > 0) {
+                warning = Ext.getCmp('warning');
+                warning.title = "In a protected area";
+                warning.ui = 'dark';
+            } else {
+                warning = Ext.getCmp('warning');
+                warning.title = "Not in a protected area";
+                warning.ui = 'light';
+            }
+        }
+    });
+
+}
+
 var init = function () {
 
     var geolocate = new OpenLayers.Control.Geolocate({
         id: 'locate-control',
         watch: true,
         geolocationOptions: {
-            enableHighAccuracy: false,
-            maximumAge: 100000
-            //timeout: 7000
+            enableHighAccuracy: true,
+            maximumAge: 10000,
+            timeout: 600000
         }
     });
-    
- 
+
 
     geolocate.events.register("locationupdated", this, function(e) {
         vector.removeAllFeatures();
         vector.addFeatures([
-        new OpenLayers.Feature.Vector(
-            e.point,
+            new OpenLayers.Feature.Vector(
+                    e.point,
             {},
             {
                 graphicName: 'cross',
@@ -72,10 +72,11 @@ var init = function () {
                 fillOpacity: 0,
                 pointRadius: 10
             }
-        )]);
+                    )]);
         map.setCenter(vector.getDataExtent().getCenterLonLat());
-        if (map.zoom == initial_zoom_level)
+        if (map.zoom === initial_zoom_level) {
             map.zoomTo(9);
+        }
         checkIsInLayer(vector.getDataExtent());
     });
 
@@ -105,15 +106,15 @@ var init = function () {
     };
 
     var protectedArea = new OpenLayers.Layer.WMTS(Ext.applyIf(
-        {name: 'Wildruhe',
+    {name: 'Wildruhe',
         layer: LAYER_NAME,
         isBaseLayer: false,
         formatSuffix: 'png',
-        attribution: 'BAFU'}, 
-        wmts_options));
+        attribution: 'BAFU'},
+            wmts_options));
 
 
-    var wmts = new OpenLayers.Layer.WMTS( Ext.applyIf({
+    var wmts = new OpenLayers.Layer.WMTS(Ext.applyIf({
         name: "Pixelkarte (WMTS - controller)",
         layer: "ch.swisstopo.pixelkarte-farbe",
         format: "image/jpeg",
@@ -122,7 +123,7 @@ var init = function () {
         isBaseLayer: true,
         formatSuffix: 'jpeg',
         attribution: 'swisstopo'
-    }, wmts_options)); 
+    }, wmts_options));
 
     // create map
     map = new OpenLayers.Map({
