fdaa2cdee3be1429005a8f32a49dbfb68669343d
ltmom <ltmom@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2011-04-27 15:34:16 +0000

    fixing Ext falsely advertising IE9 as IE6 (loosely refs #2519 and refs #2295)
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@1145 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/api/ie9.js
+++ chsdi/public/api/ie9.js
@@ -5,14 +5,34 @@
  * To be remove once we use an ExtJS version post 15 March 2011
  *
  */
-if ((typeof Range !== "undefined") && !Range.prototype.createContextualFragment)
-{
-	Range.prototype.createContextualFragment = function(html)
-	{
-		var frag = document.createDocumentFragment(), 
-		div = document.createElement("div");
-		frag.appendChild(div);
-		div.outerHTML = html;
-		return frag;
-	};
+if ((typeof Range !== "undefined") && !Range.prototype.createContextualFragment) {
+    Range.prototype.createContextualFragment = function(html) {
+        var frag = document.createDocumentFragment(),
+                div = document.createElement("div");
+        frag.appendChild(div);
+        div.outerHTML = html;
+        return frag;
+    };
 }
+
+/* FIXME
+ * Ext falsely advertise IE9 as IE6!
+ * Will be hopefully fixed in Ext 3.3.3
+*/
+
+if (Ext) {
+
+    var isIE = window.ActiveXObject ? true : false;
+    var ieVer = navigator.userAgent.match(/msie (\d+)/i);
+
+    Ext.apply(Ext, {
+        isIE : true,
+        isIE6: isIE && ieVer <= 6,
+        isIE7:  isIE && ieVer == 7,
+        isIE8: isIE && ieVer >= 8
+    });
+}
+
+
+
+
