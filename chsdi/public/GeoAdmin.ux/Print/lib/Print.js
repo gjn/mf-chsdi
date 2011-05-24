722b6115f0c2fd844f19950ab778b5050d49174d
ltmoc <ltmoc@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2011-05-24 12:58:27 +0000

    Overwrite the GeoExt encoders in order to support the unused zoom level of WMTS
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@1270 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/GeoAdmin.ux/Print/lib/Print.js
+++ chsdi/public/GeoAdmin.ux/Print/lib/Print.js
@@ -324,6 +324,207 @@ GeoAdmin.Print = Ext.extend(Ext.Action, {
                         scope: this
                     });
                 }
+            },
+
+            /** private: property[encoders]
+             *  ``Object`` Encoders for all print content
+             */
+            encoders: {
+                "layers": {
+                    "WMS": function(layer) {
+                        var enc = this.encoders.layers.HTTPRequest.call(this, layer);
+                        Ext.apply(enc, {
+                            type: 'WMS',
+                            layers: [layer.params.LAYERS].join(",").split(","),
+                            format: layer.params.FORMAT,
+                            styles: [layer.params.STYLES].join(",").split(",")
+                        });
+                        var param;
+                        for (var p in layer.params) {
+                            param = p.toLowerCase();
+                            if (!layer.DEFAULT_PARAMS[param] &&
+                                    "layers,styles,width,height,srs".indexOf(param) == -1) {
+                                if (!enc.customParams) {
+                                    enc.customParams = {};
+                                }
+                                enc.customParams[p] = layer.params[p];
+                            }
+                        }
+                        return enc;
+                    },
+                    "OSM": function(layer) {
+                        var enc = this.encoders.layers.TileCache.call(this, layer);
+                        return Ext.apply(enc, {
+                            type: 'OSM',
+                            baseURL: enc.baseURL.substr(0, enc.baseURL.indexOf("$")),
+                            extension: "png"
+                        });
+                    },
+                    "TMS": function(layer) {
+                        var enc = this.encoders.layers.TileCache.call(this, layer);
+                        return Ext.apply(enc, {
+                            type: 'TMS',
+                            format: layer.type
+                        });
+                    },
+                    "TileCache": function(layer) {
+                        var enc = this.encoders.layers.HTTPRequest.call(this, layer);
+                        return Ext.apply(enc, {
+                            type: 'TileCache',
+                            layer: layer.layername,
+                            maxExtent: layer.maxExtent.toArray(),
+                            tileSize: [layer.tileSize.w, layer.tileSize.h],
+                            extension: layer.extension,
+                            resolutions: layer.serverResolutions || layer.resolutions
+                        });
+                    },
+                    "WMTS": function(layer) {
+                        var enc = this.encoders.layers.HTTPRequest.call(this, layer);
+                        return Ext.apply(enc, {
+                            type: 'WMTS',
+                            layer: layer.layer,
+                            version: layer.version,
+                            requestEncoding: layer.requestEncoding,
+                            tileOrigin: [layer.tileOrigin.lon, layer.tileOrigin.lat],
+                            tileSize: [layer.tileSize.w, layer.tileSize.h],
+                            style: layer.style,
+                            formatSuffix: layer.formatSuffix,
+                            dimensions: layer.dimensions,
+                            params: layer.params,
+                            maxExtent: (layer.tileFullExtent != null) ? layer.tileFullExtent.toArray() : layer.maxExtent.toArray(),
+                            matrixSet: layer.matrixSet,
+                            zoomOffset: layer.zoomOffset,
+                            resolutions: layer.serverResolutions || [650,500,250,100,50,20,10,5,2.5,2,1.5,1,0.5]
+                        });
+                    },
+                    "KaMapCache": function(layer) {
+                        var enc = this.encoders.layers.KaMap.call(this, layer);
+                        return Ext.apply(enc, {
+                            type: 'KaMapCache',
+                            // group param is mandatory when using KaMapCache
+                            group: layer.params['g'],
+                            metaTileWidth: layer.params['metaTileSize']['w'],
+                            metaTileHeight: layer.params['metaTileSize']['h']
+                        });
+                    },
+                    "KaMap": function(layer) {
+                        var enc = this.encoders.layers.HTTPRequest.call(this, layer);
+                        return Ext.apply(enc, {
+                            type: 'KaMap',
+                            map: layer.params['map'],
+                            extension: layer.params['i'],
+                            // group param is optional when using KaMap
+                            group: layer.params['g'] || "",
+                            maxExtent: layer.maxExtent.toArray(),
+                            tileSize: [layer.tileSize.w, layer.tileSize.h],
+                            resolutions: layer.serverResolutions || layer.resolutions
+                        });
+                    },
+                    "HTTPRequest": function(layer) {
+                        return {
+                            baseURL: this.getAbsoluteUrl(layer.url instanceof Array ?
+                                    layer.url[0] : layer.url),
+                            opacity: (layer.opacity != null) ? layer.opacity : 1.0,
+                            singleTile: layer.singleTile
+                        };
+                    },
+                    "Image": function(layer) {
+                        return {
+                            type: 'Image',
+                            baseURL: this.getAbsoluteUrl(layer.getURL(layer.extent)),
+                            opacity: (layer.opacity != null) ? layer.opacity : 1.0,
+                            extent: layer.extent.toArray(),
+                            pixelSize: [layer.size.w, layer.size.h],
+                            name: layer.name
+                        };
+                    },
+                    "Vector": function(layer) {
+                        if (!layer.features.length) {
+                            return;
+                        }
+
+                        var encFeatures = [];
+                        var encStyles = {};
+                        var features = layer.features;
+                        var featureFormat = new OpenLayers.Format.GeoJSON();
+                        var styleFormat = new OpenLayers.Format.JSON();
+                        var nextId = 1;
+                        var styleDict = {};
+                        var feature, style, dictKey, dictItem, styleName;
+                        for (var i = 0, len = features.length; i < len; ++i) {
+                            feature = features[i];
+                            style = feature.style || layer.style ||
+                                    layer.styleMap.createSymbolizer(feature,
+                                            feature.renderIntent);
+                            dictKey = styleFormat.write(style);
+                            dictItem = styleDict[dictKey];
+                            if (dictItem) {
+                                //this style is already known
+                                styleName = dictItem;
+                            } else {
+                                //new style
+                                styleDict[dictKey] = styleName = nextId++;
+                                if (style.externalGraphic) {
+                                    encStyles[styleName] = Ext.applyIf({
+                                        externalGraphic: this.getAbsoluteUrl(
+                                                style.externalGraphic)}, style);
+                                } else {
+                                    encStyles[styleName] = style;
+                                }
+                            }
+                            var featureGeoJson = featureFormat.extract.feature.call(
+                                    featureFormat, feature);
+
+                            featureGeoJson.properties = OpenLayers.Util.extend({
+                                _gx_style: styleName
+                            }, featureGeoJson.properties);
+
+                            encFeatures.push(featureGeoJson);
+                        }
+
+                        return {
+                            type: 'Vector',
+                            styles: encStyles,
+                            styleProperty: '_gx_style',
+                            geoJson: {
+                                type: "FeatureCollection",
+                                features: encFeatures
+                            },
+                            name: layer.name,
+                            opacity: (layer.opacity != null) ? layer.opacity : 1.0
+                        };
+                    }
+                },
+                "legends": {
+                    "gx_wmslegend": function(legend) {
+                        var enc = this.encoders.legends.base.call(this, legend);
+                        var icons = [];
+                        for (var i = 1, len = legend.items.getCount(); i < len; ++i) {
+                            icons.push(this.getAbsoluteUrl(legend.items.get(i).url));
+                        }
+                        enc[0].classes[0] = {
+                            name: "",
+                            icons: icons
+                        };
+                        return enc;
+                    },
+                    "gx_urllegend": function(legend) {
+                        var enc = this.encoders.legends.base.call(this, legend);
+                        enc[0].classes.push({
+                            name: "",
+                            icon: this.getAbsoluteUrl(legend.items.get(1).url)
+                        });
+                        return enc;
+                    },
+                    "base": function(legend) {
+                        return [
+                            {
+                                name: legend.items.get(0).text,
+                                classes: []
+                            }
+                        ];
+                    }
+                }
             }
 
         });
