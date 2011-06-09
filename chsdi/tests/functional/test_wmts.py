5da30ed17b585355fb194b1032961baea34aa2fb
ltmom <ltmom@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2011-06-09 10:19:10 +0000

    adds xml validation of the returned wmts getcapabilities document
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@1327 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/tests/functional/test_wmts.py
+++ chsdi/tests/functional/test_wmts.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import os
 import re
+import tempfile
+import subprocess
 
 import xml.dom.minidom
 
@@ -14,7 +17,27 @@ class TestWmtsController(TestController):
 
         dom = xml.dom.minidom.parseString( resp.response.body )
 
-
         assert resp.response.content_type == "application/xml"
         assert 'TileMatrixSet' in resp
 
+    def test_validate_getcapabilities(self):
+        schema_url="http://schemas.opengis.net/wmts/1.0/wmtsGetCapabilities_response.xsd"
+        os.environ['XML_CATALOG_FILES'] = os.path.join(os.path.dirname(__file__),"xml/catalog")
+
+        for lang in ['de','fr']:
+            f = tempfile.NamedTemporaryFile(mode='w+t', prefix='WMTSCapabilities-',suffix= '-' + lang) 
+
+            resp = self.app.get(url(controller='wmts', action='index'), params={'lang': lang} )
+            f.write(resp.response.body)
+            f.seek(0)
+            retcode = subprocess.call(["xmllint", "--noout", "--catalogs","--schema", schema_url, f.name ])
+            f.close()
+
+            assert retcode == 0
+
+    def test_gettile(self):
+        resp = self.app.get(url(controller='wmts', action='index'), )
+
+        dom = xml.dom.minidom.parseString( resp.response.body )
+
+        assert resp.response.content_type == "application/xml"
