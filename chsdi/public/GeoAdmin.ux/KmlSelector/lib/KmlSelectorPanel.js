10a7430001ae4d821aa00dfa4065760f81e3d831
ltmoc <ltmoc@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2011-07-30 04:49:55 +0000

    Add KmlSelector UX. Initial structure
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@1479 e100092c-b5e6-494f-8bdf-19c1ff2a4001

---
+++ chsdi/public/GeoAdmin.ux/KmlSelector/lib/KmlSelectorPanel.js
@@ -0,0 +1,13 @@
+/*global GeoAdmin:true, OpenLayers: true, Ext:true */
+
+
+/**
+ * @include OpenLayers/Control/ArgParser.js
+ * @include OpenLayers/Format/KML.js
+ */
+
+Ext.namespace("GeoAdmin");
+
+GeoAdmin.KmlSelectorPanel = Ext.extend(Ext.Panel, {
+});
+
