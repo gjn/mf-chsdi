932b0d66e625932999759ddafe96e1dcfe72f7ee
c2cpierre <c2cpierre@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2011-10-03 14:59:10 +0000

    Fixing indentation
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@1641 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/GeoAdmin.ux/LayerTree/lib/LayerNode.js
+++ chsdi/public/GeoAdmin.ux/LayerTree/lib/LayerNode.js
@@ -19,11 +19,11 @@
 GeoAdmin.LayerNode = Ext.extend(GeoExt.tree.LayerNode, {
 
     constructor: function(config) {
-            if(config.layer.geoadmin_queryable){
-                config.iconCls = 'tree-layer-icon-queryable';
-            }else{
-                config.iconCls = 'tree-layer-icon';
-            }
+        if(config.layer.geoadmin_queryable){
+            config.iconCls = 'tree-layer-icon-queryable';
+        }else{
+            config.iconCls = 'tree-layer-icon';
+        }
         GeoAdmin.LayerNode.superclass.constructor.apply(this, arguments);
     },
 
