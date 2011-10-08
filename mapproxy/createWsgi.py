e2bcaca0705c635414eaee46c5f8bf8658abe98f
ltmom <ltmom@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2011-10-08 15:41:55 +0000

    adding MapProxy to the project + make it deployable
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@1676 e100092c-b5e6-494f-8bdf-19c1ff2a4001

---
+++ mapproxy/createWsgi.py
@@ -0,0 +1,57 @@
+
+import sys
+import os.path
+import glob
+
+WRAPPER_TEMPLATE = """\
+# This is a generated file. It will be overwritten by the next buildout command.
+
+import sys
+import os.path
+
+syspaths = [
+       %(syspath)s,
+]
+
+for path in reversed(syspaths):
+   if path not in sys.path:
+         sys.path[0:0]=[path]
+
+
+from paste.deploy import loadapp
+from paste.script.util.logging_config import fileConfig
+
+# If you want to debug, uncomment the following line
+#fileConfig(r'%(log_config)s', {'here': os.path.dirname(__file__)})
+
+
+from mapproxy.wsgiapp import make_wsgi_app
+configfile="%(config)s"
+application = make_wsgi_app(configfile)
+
+"""
+
+
+__here__ = os.path.abspath(os.path.dirname(__file__) )
+buildout_dir = os.path.dirname(__here__) 
+eggs_dir = os.path.join(buildout_dir, 'buildout/eggs')
+
+syspaths = [eggs_dir]
+syspaths.extend( glob.glob(eggs_dir +  '/*.egg'))
+
+output=WRAPPER_TEMPLATE % dict(
+              config= os.path.join(buildout_dir, 'mapproxy/mapproxy.yaml'),
+              syspath=",\n    ".join((repr(p) for p in syspaths)),
+              log_config = os.path.join(buildout_dir, 'mapproxy/log.ini')
+          )
+
+location=os.path.abspath(os.path.join(buildout_dir, 'buildout/parts/mapproxy'))
+
+if not os.path.exists(location):
+              os.mkdir(location)
+target=os.path.join(location, "wsgi")
+print target
+f=open(target, "wt")
+f.write(output)
+f.close()
+os.chmod(target, 0755)
