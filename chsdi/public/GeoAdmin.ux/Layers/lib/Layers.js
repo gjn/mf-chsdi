11c002e08733d06905b5f6cf771e2ade1f9907ce
ltret <ltret@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2011-11-08 09:08:03 +0000

    4 update mf-are2 oev_gueteklassen
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@1791 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/GeoAdmin.ux/Layers/lib/Layers.js
+++ chsdi/public/GeoAdmin.ux/Layers/lib/Layers.js
@@ -1494,6 +1494,7 @@ GeoAdmin._Layers = OpenLayers.Class({
                 opacity: 0.75,
                 queryable: true
             },
+            /*  until tileworker works again ltret
             "ch.are.gueteklassen_oev": {
                 name: OpenLayers.i18n("ch.are.gueteklassen_oev"),
                 layertype: 'wmts',
@@ -1504,6 +1505,18 @@ GeoAdmin._Layers = OpenLayers.Class({
                 opacity: 0.75,
                 queryable: true
             },
+            */
+             "ch.are.gueteklassen_oev": {
+                name: OpenLayers.i18n("ch.are.gueteklassen_oev"),
+                layers: ["ch.are.gueteklassen_oev"],
+                layertype: 'wms',
+                type: "polygon",
+                format: "image/png",
+                datenherr: "ch.are",
+                opacity: 0.5,
+                queryable: true
+             },
+
             "ch.are.reisezeit_miv-2005": {
                 name: OpenLayers.i18n("ch.are.reisezeit_miv-2005"),
                 layertype: 'wmts',
