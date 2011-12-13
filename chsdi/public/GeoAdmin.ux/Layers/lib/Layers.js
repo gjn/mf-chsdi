e0e61dfa3e393e4aef974ed392b1e1cec409ee05
ltfoa <ltfoa@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2011-12-13 07:47:24 +0000

    adapted timestamp swisstopo-vd.ortschaftenverzeichnis_plz (nachführung)
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@1876 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/GeoAdmin.ux/Layers/lib/Layers.js
+++ chsdi/public/GeoAdmin.ux/Layers/lib/Layers.js
@@ -1883,7 +1883,7 @@ GeoAdmin._Layers = OpenLayers.Class({
             "ch.swisstopo-vd.ortschaftenverzeichnis_plz": {
                 name: OpenLayers.i18n("ch.swisstopo-vd.ortschaftenverzeichnis_plz"),
                 layertype: 'wmts',
-                timestamp: '20110502',
+                timestamp: '20111101',
                 type: "polygon",
                 format: "image/png",
                 datenherr: "ch.swisstopo",
