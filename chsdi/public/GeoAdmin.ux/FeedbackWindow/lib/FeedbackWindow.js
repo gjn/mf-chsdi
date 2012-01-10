cf6b8ef7f5ba4f957e0cb6ca65cd63b38af7af87
ltgal <ltgal@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2012-01-10 14:52:21 +0000

    target blank for revision
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@1919 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/GeoAdmin.ux/FeedbackWindow/lib/FeedbackWindow.js
+++ chsdi/public/GeoAdmin.ux/FeedbackWindow/lib/FeedbackWindow.js
@@ -146,7 +146,6 @@ GeoAdmin.FeedbackWindow = Ext.extend(Ext.Window, {
     },
 
     onProviderStatechange: function(provider) {
-        this.str = OpenLayers.i18n('revision service of swisstopo');
-        this.label.setText(OpenLayers.i18n('The following URL will be transferred:') + '<a href="' + this.getPermalink() + '" target="new">' + this.getPermalink() + '</a>' + '</br></br>' + OpenLayers.i18n("For a detailed revision of the national map: ") + '</br>' + OpenLayers.i18n("Use the ") + this.str.link("http://map.revision.admin.ch/?Y=" + map.getCenter().lon + "&X=" + map.getCenter().lat + "&zoom=" + map.getZoom()), false);
+        this.label.setText(OpenLayers.i18n('The following URL will be transferred:') + '<a href="' + this.getPermalink() + '" target="new">' + this.getPermalink() + '</a>' + '</br></br>' + OpenLayers.i18n("For a detailed revision of the national map: ") + '</br>' + OpenLayers.i18n("Use the ") + '<a href="http://map.revision.admin.ch/?Y='  + map.getCenter().lon + '&X=' + map.getCenter().lat + '&zoom=' + map.getZoom() + '" target="_blank">' + OpenLayers.i18n('revision service of swisstopo') + '</a>', false);
     }
 });
