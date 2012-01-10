7d26f523b151f9daf234b7d8effcc5c943b81373
ltmoc <ltmoc@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2012-01-10 12:37:39 +0000

    Change copyrights date
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@1913 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/doc/source/conf.py
+++ chsdi/public/doc/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'GeoAdmin API'
-copyright = u'2010, COGIS'
+copyright = u'2012, COGIS'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
