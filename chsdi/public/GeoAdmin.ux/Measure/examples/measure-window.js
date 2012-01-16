63a1daf572cbd099684777413093f60491cf8171
ltmom <ltmom@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2012-01-16 15:45:58 +0000

    making measure example look nicer
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@1939 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/GeoAdmin.ux/Measure/examples/measure-window.js
+++ chsdi/public/GeoAdmin.ux/Measure/examples/measure-window.js
@@ -1,5 +1,7 @@
 /*global Ext, OpenLayers, GeoExt, GeoAdmin*/
 
+GeoAdmin.OpenLayersImgPath = "../../Map/img/";
+
 Ext.onReady(function() {
     var map = new GeoAdmin.Map();
     var mapPanel = new GeoAdmin.MapPanel({
