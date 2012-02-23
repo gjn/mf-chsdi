733fa6f81d75a79cc3fc1bf03fd376376ca30df6
ltmom <ltmom@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2012-02-23 18:41:07 +0000

    superoverlay: add second catalog tree
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@2091 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/demo/usability/Map.js
+++ chsdi/public/demo/usability/Map.js
@@ -2,50 +2,147 @@ Ext.namespace('App');
 
 App.catalogConfig = {
     'catalog1':
- {nodeType: 'async',
-  children: 
- [
-    
-        {
-            text: 'Freizeitkarten',
-            children: [
+    {nodeType: 'async',
+        children:
+            [
+
                 {
-                    text: 'Wanderkarten',
-                    layerId: "ch.swisstopo.fixpunkte-agnes",
-                    leaf: true
+                    text: 'Freizeitkarten',
+                    children: [
+                        {
+                            text: 'Wanderkarten',
+                            layerId: "ch.swisstopo.fixpunkte-agnes",
+                            leaf: true
+                        }
+                    ]
+                },
+                {
+                    text: 'Landeskarten',
+                    children: [
+                        {layerId: 'ch.swisstopo.pixelkarte-pk25.metadata', leaf: true},
+                        {layerId: 'ch.swisstopo.pixelkarte-pk50.metadata', leaf: true},
+                        {layerId: 'ch.swisstopo.pixelkarte-pk100.metadata', leaf: true},
+                        {layerId: 'ch.swisstopo.pixelkarte-pk200.metadata', leaf:true},
+                        {layerId: 'ch.swisstopo.pixelkarte-pk300.metadata', leaf: true},
+                        {layerId: 'ch.swisstopo.pixelkarte-pk500.metadata', leaf: true}
+                    ]
                 }
             ]
-        },
-        {
-            text: 'Landeskarten',
-            children: [
-                {layerId: 'ch.swisstopo.pixelkarte-pk25.metadata', leaf: true},
-                {layerId: 'ch.swisstopo.pixelkarte-pk50.metadata', leaf: true},
-                {layerId: 'ch.swisstopo.pixelkarte-pk100.metadata', leaf: true},
-                {layerId: 'ch.swisstopo.pixelkarte-pk200.metadata', leaf:true},
-                {layerId: 'ch.swisstopo.pixelkarte-pk300.metadata', leaf: true},
-                {layerId: 'ch.swisstopo.pixelkarte-pk500.metadata', leaf: true}
-            ]
-        }
-    ]
     }
     ,
     'catalog2': {
-          nodeType: 'async',
-          children: [
-        {
-            text: 'Freizeit',
-            children: [
-                {layerId: 'ch.swisstopo.vec25-wander'}
-            ]
-        },
-        {
-            text: 'Landschaftsmerkmale & -informationen',
-            children: [
-                {layerId: 'ch.swisstopo.gg25-gemeinde-flaeche.fill'}
-            ]
-        }
-    ]
+        nodeType: 'async',
+        children: [
+            {
+                text: 'Freizeit',
+                children: [
+                    {
+                        layerId: 'ch.swisstopo.vec25-wander',
+                        leaf: true
+                    }
+                ]
+            },
+            {
+                text: 'Landschaftsmerkmale & -informationen',
+                children: [
+                    {
+                        layerId: 'ch.swisstopo.gg25-gemeinde-flaeche.fill',
+                        leaf: true
+                    },
+                    {
+                        text: 'VECTOR25',
+                        children: [
+                            {
+                                layerId: 'ch.swisstopo.vec25-primaerflaechen',
+                                leaf: true
+                            }  ,
+                            {
+                                layerId: 'ch.swisstopo.vec25-gebaeude',
+                                leaf: true
+                            },
+                            {
+                                layerId: 'ch.swisstopo.vec25-gewaessernetz',
+                                leaf: true
+                            } ,
+                            {
+                                layerId: 'ch.swisstopo.vec25-eisenbahnnetz',
+                                leaf:true
+                            },
+                            {
+                                layerId: 'ch.swisstopo.vec25-wander',
+                                leaf: true
+                            },
+                            {
+                                layerId: 'ch.swisstopo.vec25-anlagen',
+                                leaf: true
+                            }
+
+                        ]
+                    },
+                    {
+                        text:'VECTOR200',
+                        children: [
+                            {
+                                layerId: 'ch.swisstopo.vec200-landcover',
+                                leaf: true
+                            },
+                            {
+                                layerId: 'ch.swisstopo.vec200-building',
+                                leaf: true
+                            },
+                            {
+                                layerId: 'ch.swisstopo.vec200-hydrography',
+                                leaf: true
+                            },
+                            {
+                                layerId: 'ch.swisstopo.vec200-transportation-strassennetz',
+                                leaf: true
+                            } ,
+                            {
+                                layerId: 'ch.swisstopo.vec200-transportation-oeffentliche-verkehr',
+                                leaf: true
+                            },
+                            {
+                                layerId: 'ch.swisstopo.vec200-miscellaneous',
+                                leaf: true
+                            },
+                            {
+                                layerId: 'ch.swisstopo.vec200-adminboundaries-protectedarea',
+                                leaf: true
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                text: 'Historischen Karten',
+                children: [
+                    {
+                        layerId: 'ch.swisstopo.hiks-dufour',
+                        leaf: true
+                    }
+
+                ]
+            } ,
+            {
+                text: 'Geologie & Relief',
+                children: [
+                    {
+                        layerId: 'ch.swisstopo.geologie-geologischer_atlas',
+                        leaf: true
+                    },
+                    {
+                        layerId: 'ch.swisstopo.geologie-eiszeit-lgm-raster',
+                        leaf: true
+                    },
+                    {
+                        layerId: 'ch.swisstopo.geologie-geotechnik-gk500-gesteinsklassierung',
+                        leaf:true
+                    }
+
+                ]
+            }
+        ]
     }
 };
 
@@ -96,23 +193,6 @@ App.Map = function(options) {
             }
         }
     });
-    var printtool = 
-new GeoAdmin.Print({
-          configureTitle: true,
-          configureFooter: true,
-          configureLegend: true,
-          mapLogo: "http://www.dummy.com/myimage.png",
-          mapTitle: "My custom title",
-          mapFooter: "This is a custom footer.",
-          text: OpenLayers.i18n('print map (popup)'),
-          printPanelOptions: {
-              mapPanel: this.mapPanel
-          },
-          windowOptions: {
-                height: 300,
-                title: OpenLayers.i18n('print map')
-          }
-       });
-    // this.mapPanel.getTopToolbar().add(printtool);
+
     tools.addToToolbar(this.mapPanel.getTopToolbar());
 }
