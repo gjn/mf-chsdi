e01b1477f7c2bc077dcdbbe9a77251dca8cb27ea
ltmom <ltmom@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2012-04-04 09:09:24 +0000

    add two layers with queryable attributes, indexes must be avaliablable in the db (refs #2880)
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@2199 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/model/vector/ivs2b.py
+++ chsdi/model/vector/ivs2b.py
@@ -3,11 +3,13 @@ from chsdi.model import *
 Base = declarative_base(bind=meta.engines['ivs2b'])
 
 class IVS_NAT(Base, Queryable):
- 	__tablename__ = 'nat_aggregated'
- 	__table_args__ = ({'autoload': True})
- 	__template__ = 'tooltips/ivs_nat.mako'
- 	id = Column('oid', Integer, primary_key=True)
- 	the_geom = Column(Geometry(21781))
+    __tablename__ = 'nat_aggregated'
+    __table_args__ = ({'autoload': True})
+    __template__ = 'tooltips/ivs_nat.mako'
+    __queryable_attributes__ = ['ivs_slaname','ivs_nummer','ivs_signatur']
+
+    id = Column('oid', Integer, primary_key=True)
+    the_geom = Column(Geometry(21781))
 
 register('ch.astra.ivs-nat', IVS_NAT)
 
