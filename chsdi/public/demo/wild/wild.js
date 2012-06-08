78b28508b260a1a94f1aed1b67b11a81da06e4e5
ltmom <ltmom@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2012-06-08 14:05:03 +0000

    print multitemplate example (refs #3856)
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@2437 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/public/demo/wild/wild.js
+++ chsdi/public/demo/wild/wild.js
@@ -1,9 +1,19 @@
 // You need to put the print button outside the ini
 var api, wrmap, buttonPrint;
 
+
+
+function qualifyURL(url){
+    var img = document.createElement('img');
+    img.src = url; 
+    url = img.src; 
+    img.src = null; 
+    return url.replace(/\/$/, "");;
+}
+
 window.GeoAdmin.version="2609111920";
-window.GeoAdmin.OpenLayersImgPath="http://api.geo.admin.ch/2609111920/main/wsgi/GeoAdmin.ux/Map/img/";
-window.GeoAdmin.webServicesUrl="http://mf-chsdi0t.bgdi.admin.ch/ltgal";
+window.GeoAdmin.OpenLayersImgPath="../../GeoAdmin.ux/Map/img/";
+window.GeoAdmin.webServicesUrl= "../../..";
 
 
 function init() {
@@ -36,11 +46,11 @@ function init() {
 		   height: 300,
 		   title: 'Print map'
 		},
-		//renderTo: 'wrmapbuttons',
+		renderTo: 'wrmapbuttons',
 		configureTitle: true, 
-		mapTitle: "My custom title",
+		mapTitle: "Wildruhezonen Schweiz"
 		// You need to set a mapLogo per default
-		mapLogo: 'http://www.wild.uzh.ch/pi/ti/wildtier_logo_gruen.gif'
+		//mapLogo: 'http://www.wild.uzh.ch/pi/ti/wildtier_logo_gruen.gif'
 	});
   
 	this.buttonPrint = new Ext.Button(printAction);
@@ -48,8 +58,9 @@ function init() {
 	buttonPrint.baseAction.printProvider.on('beforeprint', function() {
 		
 	    var formatPrint = buttonPrint.baseAction.printProvider.layout.data.label;
+	     buttonPrint.baseAction.printProvider.customParams.app = 'wrz';
 	    
-	    if (formatPrint === 'A4 Querformat') {
+	    /* if (formatPrint === 'A4 Querformat') {
 	        buttonPrint.baseAction.printProvider.customParams.mapLogo = 'http://www.wild.uzh.ch/pi/ti/wildtier_logo_gruen.gif';
 	    } else if (formatPrint === 'A4 Hochformat') {
 	        buttonPrint.baseAction.printProvider.customParams.mapLogo = 'http://mf1t.bgdi.admin.ch/~ltgal/515x100.png';
@@ -57,7 +68,7 @@ function init() {
 	        buttonPrint.baseAction.printProvider.customParams.mapLogo = 'http://mf1t.bgdi.admin.ch/~ltgal/762x100.png';      
 	    } else if (formatPrint === 'A3 Hochformat') {                      
 	        buttonPrint.baseAction.printProvider.customParams.mapLogo = 'http://dribbble.com/system/users/1130/screenshots/11898/log_rendered2.png';
-	    }
+	    } */
 	});                                                                  
 	wrmap.map.setCenter(new OpenLayers.LonLat(672000,180000),1);
 }
