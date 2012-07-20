c486fa0b021bece21e023bc3153830fb5b9c6225
ltmom <ltmom@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2012-07-20 07:51:26 +0000

    initial commit for RE3 mockup
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@2556 e100092c-b5e6-494f-8bdf-19c1ff2a4001

---
+++ chsdi/public/demo/re3.js
@@ -0,0 +1,68 @@
+var map,
+swisssearch,
+layertool;
+var $;
+
+if (!window.GeoAdmin) {
+    window.GeoAdmin = {};
+}
+
+function init() {
+    $ = OpenLayers.Util.getElement;
+    map = new GeoAdmin.Map("mymap", {});
+    map.switchComplementaryLayer('ch.swisstopo.pixelkarte-farbe', {
+        opacity: 100
+    });
+    map.zoomTo(3);
+    var control = new OpenLayers.Control.MousePosition({
+        numDigits: 0,
+        prefix: OpenLayers.i18n("Coordinates (m):")
+        });
+    map.addControl(control);
+    map.events.register("mousemove", map, function(e) {
+        var position = this.events.getMousePosition(e);
+        OpenLayers.Util.getElement("coords").innerHTML = map.getLonLatFromPixel(position);
+    });
+    swisssearch = new GeoAdmin.SwissSearchCombo('swisssearch-div', {
+        map: map
+    });
+
+    layertool = new GeoAdmin.LayerCombo({
+        div: 'layertool',
+        map: map
+    });
+
+}
+
+GeoAdmin.LayerCombo = OpenLayers.Class({
+    div: null,
+    map: null,
+
+    initialize: function(options) {
+        this.div = OpenLayers.Util.getElement(options.div);
+        this.map = options.map;
+        var layers = options.layers || ["ch.swisstopo.pixelkarte-farbe", "ch.swisstopo.pixelkarte-grau", "ch.kantone.hintergrund-farbe", "ch.swisstopo.tml3d-hintergrund-karte", "voidLayer"];
+        var selectEl = document.createElement('select');
+        selectEl.id = 'backgroundlayer';
+
+        for (var i = 0, len = layers.length; i < len; i++) {
+            id = layers[i];
+            //   if (GeoAdmin.layers.layers[id]) {
+            var optionEl = document.createElement('option');
+            optionEl.id = id;
+            optionEl.value = id;
+            optionEl.innerHTML = OpenLayers.i18n(id);
+            selectEl.appendChild(optionEl);
+            // }
+            }
+        this.div.appendChild(selectEl);
+
+        OpenLayers.Element.addClass(this.div, 'geoadmin-layertool')
+
+            selectEl.onchange = function(event) {
+            var layerId = event.target.value;
+            options.map.switchComplementaryLayer(layerId);
+
+        }
+    }
+})
