51a9c9549f107603ccf912db3bbe02ef5aeb6475
ltmoc <ltmoc@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2012-11-06 06:21:01 +0000

    Rename Zeitreihen models. Add a zeitreihen service used to determine which years has seen changes for one position. Example request: zeitreihen?scale=250000&easting=600000&northing=200000&cb=callback
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@3011 e100092c-b5e6-494f-8bdf-19c1ff2a4001

---
+++ chsdi/controllers/zeitreihen.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from pylons import request, response, session, tmpl_context as c
+from pylons.controllers.util import abort
+
+from chsdi.lib.base import BaseController, cacheable, validate_params, render
+from chsdi.model.vector.zeitreihen import Zeitreihen_Metadata_15,Zeitreihen_Metadata_20,Zeitreihen_Metadata_21,Zeitreihen_Metadata_22
+
+from chsdi.model.meta import Session
+from array import *
+
+try:
+    from json import dumps as json_dumps
+except:
+    from simplejson import dumps as json_dumps
+
+import mimetypes
+
+log = logging.getLogger(__name__)
+
+try:
+    from json import dumps as json_dumps
+except:
+    from simplejson import dumps as json_dumps
+
+def validator_scale():
+    """ Validator for the "scale" parameter. For use with
+    the validate_params action decorator."""
+    scale = request.params.get('scale')
+    if scale is not None:
+        try:
+            scale = int(scale)
+        except ValueError:
+            return False
+    c.scale = scale
+    return True
+
+
+class ZeitreihenController(BaseController):
+    def __before__(self):
+        # default lang is 'de'
+        lang = request.params.get('lang', 'de')
+        c.lang = lang
+
+    @validate_params(validator_scale)
+    def index(self):
+
+        lon = request.params.get('easting')
+        if lon is None:
+            abort(400, "missing 'easting' parameter")
+        try:
+            lon = float(lon)
+        except:
+            abort(400, "parameter 'easting' is not a number")
+
+        lat = request.params.get('northing')
+        if lat is None:
+            abort(400, "missing 'northing' parameter")
+        try:
+            lat = float(lat)
+        except:
+            abort(400, "parameter 'northing' is not a number")
+
+        mymodel = Zeitreihen_Metadata_15
+        if c.scale > 50005 and c.scale <= 100005:
+            mymodel =  Zeitreihen_Metadata_20
+        if c.scale > 25005 and c.scale <= 50005:
+            mymodel =  Zeitreihen_Metadata_21
+        if c.scale > 1 and c.scale <= 25005:
+            mymodel =  Zeitreihen_Metadata_22
+
+        query = Session.query(mymodel)
+        spatialFilter = mymodel.within_filter(lon, lat, column='the_geom')
+        query = query.filter(spatialFilter)
+        query = query.order_by(mymodel.bgdi_order)
+        query = query.limit(1)
+
+        #Default timestamp
+        timestamps = ['1938','1950','1960','1970','1980','1990','2000','2010']
+
+        for f in query.all():
+            timestamps = f.release_year
+
+        counter = 0
+        for value in timestamps:
+            timestamps[counter] = int(str(timestamps[counter]+'1231'))
+            counter = counter+1
+
+        timestamps.sort()
+        if timestamps[0] != 19381231:
+            timestamps.append(19381231)
+        timestamps.sort()
+        if timestamps[len(timestamps)-1] != 20101231:
+            timestamps.append(20101231)
+        timestamps.sort()
+        myjson = json_dumps(timestamps)
+
+        response.headers['Cache-Control'] = 'no-cache'
+        if 'cb' in request.params:
+            response.headers['Content-Type'] = 'text/javascript; charset=utf-8'
+            return request.params['cb'] + '(' + myjson + ');'
+        else:
+            response.headers['Content-Type'] = 'application/json'
+            return myjson
--- chsdi/model/vector/zeitreihen.py
+++ chsdi/model/vector/zeitreihen.py
@@ -1,12 +1,21 @@
 from chsdi.model import *
 
+from sqlalchemy import Column
+
+from geoalchemy import Geometry
+
+from shapely.wkb import loads
+from shapely.geometry.point import Point
+
+from geoalchemy import functions
+
 Base = declarative_base(bind=meta.engines['zeitreihen'])
 
-class Zeitreihen_Metadata_15(Base, Queryable):
+class Zeitreihen_15(Base, Queryable):
     # view in a schema
     __tablename__ = 'tooltip_15'
     __table_args__ = ({'schema': 'public','autoload': True})
-    __template__ = 'tooltips/zeitreihen_metadata.mako'
+    __template__ = 'tooltips/zeitreihen.mako'
 
     id = Column('gid', Integer, primary_key=True)
     the_geom = Column('the_geom', Geometry)
@@ -17,11 +26,11 @@ class Zeitreihen_Metadata_15(Base, Queryable):
     __minscale__ = 100005
     __maxscale__ = 5000005
 
-class Zeitreihen_Metadata_20(Base, Queryable):
+class Zeitreihen_20(Base, Queryable):
     # view in a schema
     __tablename__ = 'tooltip_20'
     __table_args__ = ({'schema': 'public','autoload': True})
-    __template__ = 'tooltips/zeitreihen_metadata.mako'
+    __template__ = 'tooltips/zeitreihen.mako'
 
     id = Column('gid', Integer, primary_key=True)
     the_geom = Column('the_geom', Geometry)
@@ -32,11 +41,11 @@ class Zeitreihen_Metadata_20(Base, Queryable):
     __minscale__ = 50005
     __maxscale__ = 100005
 
-class Zeitreihen_Metadata_21(Base, Queryable):
+class Zeitreihen_21(Base, Queryable):
     # view in a schema
     __tablename__ = 'tooltip_21'
     __table_args__ = ({'schema': 'public','autoload': True})
-    __template__ = 'tooltips/zeitreihen_metadata.mako'
+    __template__ = 'tooltips/zeitreihen.mako'
 
     id = Column('gid', Integer, primary_key=True)
     the_geom = Column('the_geom', Geometry)
@@ -47,11 +56,11 @@ class Zeitreihen_Metadata_21(Base, Queryable):
     __minscale__ = 25005
     __maxscale__ = 50005
 
-class Zeitreihen_Metadata_22(Base, Queryable):
+class Zeitreihen_22(Base, Queryable):
     # view in a schema
     __tablename__ = 'tooltip_22'
     __table_args__ = ({'schema': 'public','autoload': True})
-    __template__ = 'tooltips/zeitreihen_metadata.mako'
+    __template__ = 'tooltips/zeitreihen.mako'
 
     id = Column('gid', Integer, primary_key=True)
     the_geom = Column('the_geom', Geometry)
@@ -62,7 +71,69 @@ class Zeitreihen_Metadata_22(Base, Queryable):
     __minscale__ = 24995
     __maxscale__ = 25005
 
-register('ch.swisstopo.zeitreihen', Zeitreihen_Metadata_15)
-register('ch.swisstopo.zeitreihen', Zeitreihen_Metadata_20)
-register('ch.swisstopo.zeitreihen', Zeitreihen_Metadata_21)
-register('ch.swisstopo.zeitreihen', Zeitreihen_Metadata_22)
+register('ch.swisstopo.zeitreihen', Zeitreihen_15)
+register('ch.swisstopo.zeitreihen', Zeitreihen_20)
+register('ch.swisstopo.zeitreihen', Zeitreihen_21)
+register('ch.swisstopo.zeitreihen', Zeitreihen_22)
+
+class Zeitreihen_Metadata_15(Base, Queryable):
+    # view in a schema
+    __tablename__ = 'meta_15'
+    __table_args__ = ({'schema': 'public','autoload': True})
+
+    id = Column('gid', Integer, primary_key=True)
+    the_geom = Column('the_geom', Geometry)
+
+    @classmethod
+    def within_filter(cls, lon, lat, column):
+        geom = Point(lon, lat)
+        wkb_geometry = WKBSpatialElement(buffer(geom.wkb), 21781)
+        geom_column = cls.__table__.columns.get(column)
+        return functions.within_distance(geom_column, wkb_geometry, 0)
+
+class Zeitreihen_Metadata_20(Base, Queryable):
+    # view in a schema
+    __tablename__ = 'meta_20'
+    __table_args__ = ({'schema': 'public','autoload': True})
+
+    id = Column('gid', Integer, primary_key=True)
+    the_geom = Column('the_geom', Geometry)
+
+    @classmethod
+    def within_filter(cls, lon, lat, column):
+        geom = Point(lon, lat)
+        wkb_geometry = WKBSpatialElement(buffer(geom.wkb), 21781)
+        geom_column = cls.__table__.columns.get(column)
+        return functions.within_distance(geom_column, wkb_geometry, 0)
+
+class Zeitreihen_Metadata_21(Base, Queryable):
+    # view in a schema
+    __tablename__ = 'meta_21'
+    __table_args__ = ({'schema': 'public','autoload': True})
+
+    id = Column('gid', Integer, primary_key=True)
+    the_geom = Column('the_geom', Geometry)
+
+    @classmethod
+    def within_filter(cls, lon, lat, column):
+        geom = Point(lon, lat)
+        wkb_geometry = WKBSpatialElement(buffer(geom.wkb), 21781)
+        geom_column = cls.__table__.columns.get(column)
+        return functions.within_distance(geom_column, wkb_geometry, 0)
+
+class Zeitreihen_Metadata_22(Base, Queryable):
+    # view in a schema
+    __tablename__ = 'meta_22'
+    __table_args__ = ({'schema': 'public','autoload': True})
+
+    id = Column('gid', Integer, primary_key=True)
+    the_geom = Column('the_geom', Geometry)
+
+    @classmethod
+    def within_filter(cls, lon, lat, column):
+        geom = Point(lon, lat)
+        wkb_geometry = WKBSpatialElement(buffer(geom.wkb), 21781)
+        geom_column = cls.__table__.columns.get(column)
+        return functions.within_distance(geom_column, wkb_geometry, 0)
+
+
