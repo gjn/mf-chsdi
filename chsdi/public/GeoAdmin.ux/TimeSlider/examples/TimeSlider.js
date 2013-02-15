c5fef2dd0340f252998106e26b11150a2ae006e7
ltmom <ltmom@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2013-02-15 15:10:09 +0000

    timeslider to work with the TimeControl control
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@3543 e100092c-b5e6-494f-8bdf-19c1ff2a4001

---
+++ chsdi/public/GeoAdmin.ux/TimeSlider/examples/TimeSlider.js
@@ -0,0 +1,104 @@
+var map;
+var timeSlider;
+var tc;
+
+GeoAdmin.OpenLayersImgPath = "../../Map/img/";
+
+
+var config =  
+
+// Custom layers timeEnabled and with some timestamps
+{"wms": {
+        "layer": "WMS",
+        "name": "LUBIS Bilder",
+        "layertype": "wms",
+        "layers": "lubis_bilder",
+        "alternate_title": "Main lithological groups (WMS)",
+        "copyright": "swisstopo",
+        "url": "http://wms-test0i.bgdi.admin.ch/lubis",
+        "copyright_link": "http://www.bafu.admin.ch/index.html?lang=de",
+        "id": "lubis_bilderr",
+        "hasLegend": true,
+        "isBgLayer": false,
+        "datenehrr": "swisstopo",
+        "opacity": 0.75,
+        "transparent": true,
+        "queryable": false,
+        "searchable": false,
+        "type": "polygon",
+        "minScale": 10000000,
+        "maxScale": 1,
+        "timestamp": ['1946', '1950', '1960', '1990', '1998', '1999'],
+    	"timeEnabled": true
+},
+"wmts": {
+        "layer": "ch.swisstopo.zeitreihen",
+        layername: "ch.swisstopo.zeitreihen",
+        layertype: "wmts",
+        "alternate_title": "Geological Atlas 1:25 000 (WMTS)",
+        "datenherr": "swisstopo",
+        "copyright_link": "http://www.bafu.admin.ch/index.html?lang=de",
+        "layertype": "wmts",
+        "layerinfo_url": "http://www.swisstopo.admin.ch/internet/swisstopo/fr/home/products/maps/geology/atlas.print.html",
+        "id": "wmts-custom-layer",
+        "hasLegend": true,
+        "requestEncoding": "REST",
+        dimensions: ['TIME'],
+        "url": ['http://wmts0.geo.admin.ch/'],
+        matrixSet: "21781",
+        //matrixIds: matrixIds,
+        format: "image/png",
+        name: OpenLayers.i18n("ch.swisstopo.zeitreihen"),
+       timestamp: ['20111231','20001231','19991231','19981231','19971231','19881231','19731231','19691231','19591231','19531231','19441231','19411231'],
+        type: "raster",
+        format: "image/jpeg",
+        datenherr: "ch.swisstopo",
+        queryable: true,
+        serverResolutions: [4000, 3750, 3500, 3250, 3000, 2750, 2500, 2250, 2000, 1750, 1500, 1250, 1000, 750, 650.0, 500.0, 250.0, 100.0, 50.0, 20.0, 10.0, 5.0 ,2.5],
+        opacity: 1.0,
+        isBgLayer: false,
+        "queryable": true,
+        "searchable": false,
+        "timeEnabled": true
+
+        }
+};
+
+function toggleAddRemoveLayer(layername) {
+    var lyr = map.getLayerByLayerName(layername);
+    if (lyr) { map.removeLayer(lyr); } else {
+         var lyr = map.addLayerByName(layername);
+	//map.setLayerIndex(lyr, map.layers.length );
+    };
+   
+};
+
+function log(msg) {
+    console.log(msg);
+}
+
+
+Ext.onReady(function() {
+    // init for the custom layers
+    GeoAdmin.layers.init();
+    GeoAdmin.layers.layers['ch.swisstopo.zeitreihen'] = config['wmts'];
+    GeoAdmin.layers.layers['lubis_bilder'] = config['wms'];
+    
+
+    map = new GeoAdmin.Map("map");
+    map.switchComplementaryLayer("voidLayer");
+    tc = new GeoAdmin.TimeControl({map: map});
+    timeSlider = new GeoAdmin.TimeSlider({
+        timecontrol: tc,
+        renderTo: 'timeslider',
+    });
+    
+    var layertree = new GeoAdmin.LayerTree({
+          map: map,
+          renderTo: "layertree",
+          width: 300
+      });
+    
+    map.zoomToMaxExtent();
+    timeSlider._slider.setValue(1969);
+});
--- chsdi/public/GeoAdmin.ux/TimeSlider/lib/TimeSlider.js
+++ chsdi/public/GeoAdmin.ux/TimeSlider/lib/TimeSlider.js
@@ -4,99 +4,158 @@
  * @include OpenLayers/Lang.js
  */
 
-GeoAdmin.TimeSlider = OpenLayers.Class({
+Ext.TimeSlider = Ext.extend(Ext.Slider, {
 
-    map: null,
-
-    items: null,
-
-    slider: null,
-
-    layerToAdd: null,
-
-    layerToRemove: null,
-
-    initialize: function(options) {
-        this.map = options.map;
-        this.items = [
-            this.buildTimeSlider(options),
-            this.buildVideoButton()
-        ];
-        if (options.renderTo) {
-            new Ext.Toolbar(OpenLayers.Util.extend(options, {
-                items: this.items
-            }));
+    doSnap: function(value) {
+        if (this.increments) {
+            return this.getClosestIncrement(value);
         }
-        // FIXME: not nice to use global variables
-        window.timeSlider = this;
     },
-
-    switchLayer: function() {
-        if (this.layerToAdd.opacity < 1) {
-            this.layerToAdd.setOpacity(this.layerToAdd.opacity + 0.1);
-            this.layerToRemove.setOpacity(this.layerToRemove.opacity - 0.1);
-            window.setTimeout("window.timeSlider.switchLayer()", 100);
+    getClosestIncrement: function(value) {
+        var lo,
+        hi;
+        if (value == 0) {
+            return this.increments[0];
+        }
+        if (value < this.increments[0]) {
+            return this.increments[0];
+        }
+        if (value > this.increments[this.increments.length]) {
+            return this.increments[this.increments.length]
+            }
+        for (var i = this.increments.length; i--;) {
+            if (this.increments[i] <= value && (lo === undefined || lo < this.increments[i]))
+                lo = this.increments[i];
+            if (this.increments[i] >= value && (hi === undefined || hi > this.increments[i]))
+                hi = this.increments[i];
+        };
+        if ((value - lo) > (hi - value)) {
+            return hi;
         } else {
-            this.layerToAdd.setOpacity(1);
-            this.map.removeLayer(this.layerToRemove);
+            return lo;
         }
-    },
-
-    buildTimeSlider: function(options) {
-
-        this.slider = new Ext.Slider({
-            width: 214,
-            minValue: 1,
-            maxValue: options.layerList.length,
-            layerList: options.layerList
-        });
-
-        // Add initial layer
-        this.slider.currentLayer = this.slider.layerList[0][1];
-        this.map.addLayerByName(this.slider.currentLayer);
-
-
-        this.slider.on({
-            'changecomplete': {
-                fn: function(slider, newValue) {
-                    var layer = slider.layerList[slider.getValue() - 1][1];
-                    // Get current layer
-                    this.layerToRemove = this.map.getLayersByName(slider.currentLayer)[0];
-
-                    // Add new layer
-                    this.layerToAdd = this.map.addLayerByName(layer, {
-                        opacity: 0
-                    });
-
-                    // Switch between old and new layer with a fading effect
-                    this.switchLayer();
-                },
-                scope: this
-            },'beforechange': {
-                fn: function(slider, newValue) {
-                    if (slider.getValue() > 0) {
-                        var layer = slider.layerList[slider.getValue() - 1][1];
-                        slider.currentLayer = layer;
-                    }
-                },
-                scope: this
-            }
-        });
-
-        return this.slider;
-    },
-
-    buildVideoButton: function() {
-        var box = new Ext.BoxComponent({
-            autoEl: {
-                id: 'playIcon',
-                tag: 'img',
-                src: '../img/play.png'
-            }
-        });
-        box.addListener('click', function(event, element) {
-            alert('clik');
-        });
-        return box;
     }
 });
+
+/** api: constructor
+ *  .. class:: TimeSlider(options)
+ *
+ *  :param options: ``Object`` options
+ *
+ *  :return:  ``GeoAdmin.TimeSlider
+ *
+ *  Add a time slider to the map, will be displayed if time enabled layers are present
+ */
+
+GeoAdmin.TimeSlider = Ext.extend(Ext.Container, {
+
+    timecontrol: null,
+
+    layout: 'column',
+
+    div: null,
+
+    initComponent: function() {
+
+        this.div = OpenLayers.Util.createAlphaImageDiv('timeslider', null, null, null, 'relative');
+        this.div.style.display = 'none';
+        this.div.style.width = '400px';
+        this.div.style.height = '80px';
+        this.div.style.left = '100px';
+        this.div.style.backgroundColor = '#ffffff';
+        this.div.style.opacity = 1.0;
+        this.div.style.zIndex = 2000;
+        
+
+        this._timecontrol = this.timecontrol;
+        delete this.timecontrol;
+
+        this._timecontrol.map.viewPortDiv.appendChild(this.div);
+
+        this._slider = this.createSlider();
+
+        this._slider.map = this._timecontrol.map;
+        this._timecontrol.events.on({
+            changeavailabletimestamps: this.onChangeTimestampYear,
+            scope: this
+        });
+
+        this._slider.on({
+            "change": function(slider, newValue, thumb) {
+                var slider = this.findParentByType('ga_timeslider');
+                slider._timecontrol.setMapYear(newValue);
+                
+            }
+        });
+        this.items = [
+                         this.createLabel(this._slider.minValue), 
+                         this._slider, this.createLabel(this._slider.maxValue), 
+                         new Ext.Button({
+                             icon: '../images/time.png'
+                         })
+        ];
+        GeoAdmin.TimeSlider.superclass.initComponent.apply(this, arguments);
+    },
+    createSlider: function(options) {
+        options = Ext.apply({}, options);
+
+        var currentYear = new Date().getFullYear();
+
+        return new Ext.TimeSlider({
+            id: 'geoadmin-timeslider',
+            width: 300,
+            increment: 1,
+            //increments: [1938,1947,1960,1974,1990,2006,2012],
+            minValue: 1938,
+            maxValue: currentYear,
+            value: currentYear,
+            plugins: new Ext.slider.Tip()
+            })
+        },
+
+    createLabel: function(label) {
+        return new Ext.BoxComponent({
+            html: '<span class="geoadmin-timeslider-label x-unselectable">' + label + '</span>'
+        });
+    },
+
+    createTicks: function() {
+        var el = this._slider.getEl().select('.x-slider-inner');
+        var min = this._slider.minValue;
+        var max = this._slider.maxValue
+        var increments = this._timecontrol.getTimestamps();
+        for (var i = 0; i < increments.length; i++) {
+            var incr = increments[i];
+            var px = Math.round((incr - min) / (max - min) * 286 - 5);
+            var e = el.createChild({
+                id: incr,
+                cls: 'timeslider-tick',
+                style: "left: " + px + 'px' + "; right: auto;"
+            });
+        }
+
+    },
+    removeTicks: function() {
+        var el = this._slider.getEl().select('.timeslider-tick');
+        for (var i = 0; i < el.elements.length; i++) {
+            var parentNode = el.elements[i].parentNode;
+            parentNode.removeChild(el.elements[i]);
+        }
+    },
+
+    onChangeTimestampYear: function(evt) {
+        this._slider.increments = evt.timestamps;
+        this._slider.syncThumb();
+        this.removeTicks();
+        this.createTicks();
+
+        if (this._slider.increments.length > 0) {
+            this.div.style.display = 'block';
+        } else {
+            this.div.style.display = 'none';
+        }
+
+    }
+});
+/** api: xtype = ga_timeslider */
+Ext.reg("ga_timeslider", GeoAdmin.TimeSlider);
