287b76e6bc1ceab263537acae94b7b7d11362e35
ltgud <ltgud@e100092c-b5e6-494f-8bdf-19c1ff2a4001>
2013-03-21 08:22:13 +0000

    Standardization of register - data not modified
    git-svn-id: https://svn.bgdi.admin.ch/mf-chsdi/trunk/chsdi@3669 e100092c-b5e6-494f-8bdf-19c1ff2a4001

--- chsdi/model/vector/bafu.py
+++ chsdi/model/vector/bafu.py
@@ -7,18 +7,18 @@ class AM_G(Base, Queryable):
     __tablename__ = 'am_g'
     __table_args__ = ({'schema': 'bundinv', 'autoload': False})
     __template__ = 'tooltips/am_g.mako'
-
     id = Column('am_g_obj', Integer, primary_key=True)
     am_g_obj = column_property(id)
     am_g_name = Column('am_g_name', Text)
     the_geom = Column(Geometry)
+
+register('ch.bafu.bundesinventare-amphibien_wanderobjekte', AM_G)
 
 class AM_L(Base, Queryable):
     # view in a schema
     __tablename__ = 'am_l'
     __table_args__ = ({'schema': 'bundinv', 'autoload': False})
     __template__ = 'tooltips/am_l.mako'
-
     id = Column('am_l_obj', Text, primary_key=True)
     am_l_obj = column_property(id)
     am_l_name = Column('am_l_name', Text)
@@ -27,168 +27,185 @@ class AM_L(Base, Queryable):
     am_l_gf = Column('am_l_gf', Text)
     the_geom = Column(Geometry)
 
+register('ch.bafu.bundesinventare-amphibien', AM_L)
+
 class LHG(Base, Queryable):
     # view in a schema
     __tablename__ = 'lhg'
     __table_args__ = ({'schema': 'hydrologie', 'autoload': False})
     __template__ = 'tooltips/lhg.mako'
-
     id = Column('edv_nr4', Text, primary_key=True)
     edv_nr4 = column_property(id)
     lhg_name = Column('lhg_name', Text)
     the_geom = Column(Geometry)
+
+register('ch.bafu.hydrologie-hydromessstationen', LHG)
 
 class AU(Base, Queryable):
     # view in a schema
     __tablename__ = 'au'
     __table_args__ = ({'schema': 'bundinv', 'autoload': True})
     __template__ = 'tooltips/au.mako'
-
     id = Column('gid', Integer, primary_key=True)
     the_geom = Column(Geometry)
+
+register('ch.bafu.bundesinventare-auen', AU)
 
 class BLN(Base, Queryable):
     # view in a schema
     __tablename__ = 'bln'
     __table_args__ = ({'schema': 'bundinv', 'autoload': True})
     __template__ = 'tooltips/bln.mako'
-
     id = Column('gid', Integer, primary_key=True)
     the_geom = Column(Geometry)
+
+register('ch.bafu.bundesinventare-bln', BLN)
 
 class HM(Base, Queryable):
     # view in a schema
     __tablename__ = 'hm'
     __table_args__ = ({'schema': 'bundinv', 'autoload': True})
     __template__ = 'tooltips/hm.mako'
-
     id = Column('gid', Integer, primary_key=True)
     the_geom = Column(Geometry)
+
+register('ch.bafu.bundesinventare-hochmoore', HM)
 
 class JB(Base, Queryable):
     # view in a schema
     __tablename__ = 'jb'
     __table_args__ = ({'schema': 'bundinv', 'autoload': True})
     __template__ = 'tooltips/jb.mako'
-
     id = Column('gid', Integer, primary_key=True)
     the_geom = Column(Geometry)
+
+register('ch.bafu.bundesinventare-jagdbanngebiete', JB)
 
 class ML(Base, Queryable):
     # view in a schema
     __tablename__ = 'ml'
     __table_args__ = ({'schema': 'bundinv', 'autoload': True})
     __template__ = 'tooltips/ml.mako'
-
     id = Column('gid', Integer, primary_key=True)
     the_geom = Column(Geometry)
+
+register('ch.bafu.bundesinventare-moorlandschaften', ML)
 
 class WV(Base, Queryable):
     # view in a schema
     __tablename__ = 'wv'
     __table_args__ = ({'schema': 'bundinv', 'autoload': True})
     __template__ = 'tooltips/wv.mako'
-
     id = Column('gid', Integer, primary_key=True)
     the_geom = Column(Geometry)
+
+register('ch.bafu.bundesinventare-vogelreservate', WV)
 
 class WEWB(Base, Queryable):
     # view in a schema
     __tablename__ = 'invent_ent_wknutz_bedeutend'
     __table_args__ = ({'schema': 'wasser', 'autoload': True})
     __template__ = 'tooltips/wewb.mako'
-
     id = Column('rwknr', Integer, primary_key=True)
     rwknr = column_property(id)
     the_geom = Column(Geometry)
+
+register('ch.bafu.wasser-entnahme', WEWB)
 
 class WEWW(Base, Queryable):
     # view in a schema
     __tablename__ = 'invent_ent_wknutz_weitere'
     __table_args__ = ({'schema': 'wasser', 'autoload': True})
     __template__ = 'tooltips/weww.mako'
-
     id = Column('rwknr', Integer, primary_key=True)
     rwknr = column_property(id)
     the_geom = Column(Geometry)
+
+register('ch.bafu.wasser-entnahme', WEWW)
 
 class WEANB(Base, Queryable):
     # view in a schema
     __tablename__ = 'invent_ent_andere_bedeutend'
     __table_args__ = ({'schema': 'wasser', 'autoload': True})
     __template__ = 'tooltips/weanb.mako'
-
     id = Column('rwknr', Integer, primary_key=True)
     rwknr = column_property(id)
     the_geom = Column(Geometry)
+
+register('ch.bafu.wasser-entnahme', WEANB)
 
 class WEANW(Base, Queryable):
     # view in a schema
     __tablename__ = 'invent_ent_andere_weitere'
     __table_args__ = ({'schema': 'wasser', 'autoload': True})
     __template__ = 'tooltips/weanw.mako'
-
     id = Column('rwknr', Integer, primary_key=True)
     rwknr = column_property(id)
     the_geom = Column(Geometry)
+
+register('ch.bafu.wasser-entnahme', WEANW)
 
 class WL(Base, Queryable):
     # view in a schema
     __tablename__ = 'leitungen'
     __table_args__ = ({'schema': 'wasser', 'autoload': True})
     __template__ = 'tooltips/wl.mako'
-
     id = Column('gid', Integer, primary_key=True)
     the_geom = Column(Geometry)
+
+register('ch.bafu.wasser-leitungen', WL)
 
 class WR(Base, Queryable):
     # view in a schema
     __tablename__ = 'rueckgabe'
     __table_args__ = ({'schema': 'wasser', 'autoload': True})
     __template__ = 'tooltips/wr.mako'
-
     id = Column('gid', Integer, primary_key=True)
     the_geom = Column(Geometry)
+
+register('ch.bafu.wasser-rueckgabe', WR)
 
 class FM(Base, Queryable):
     # view in a schema
     __tablename__ = 'fm'
     __table_args__ = ({'schema': 'bundinv', 'autoload': False})
     __template__ = 'tooltips/fm.mako'
-
     id = Column('gid', Integer, primary_key=True)
     fm_name = Column('fm_name', Text)
     fm_obj = Column('fm_obj', Text)
     fm_gf = Column('fm_gf', Text)
     the_geom = Column(Geometry)
 
+register('ch.bafu.bundesinventare-flachmoore', FM)
+
 class FM_REG(Base, Queryable):
     # view in a schema
     __tablename__ = 'flachmoore_regional'
     __table_args__ = ({'schema': 'bundinv', 'autoload': False})
     __template__ = 'tooltips/fm_reg.mako'
-
     id = Column('bgdi_id', Integer, primary_key=True)
     fmreg_name = Column('fmreg_name', Text)
     fmreg_obj = Column('fmreg_obj', Text)
     fmreg_gf = Column('fmreg_gf', Text)
     the_geom = Column(Geometry)
 
+register('ch.bafu.bundesinventare-flachmoore', FM_REG)
+
 class PAERKE_NATIONALER_BEDEUTUNG(Base, Queryable):
     # view in a schema
     __tablename__ = 'paerke_nationaler_bedeutung'
     __table_args__ = ({'schema': 'schutzge', 'autoload': True})
     __template__ = 'tooltips/paerke_nationaler_bedeutung.mako'
-
     id = Column('bgdi_id', Integer, primary_key=True)
     the_geom = Column(Geometry)
+
+register('ch.bafu.schutzgebiete-paerke_nationaler_bedeutung', PAERKE_NATIONALER_BEDEUTUNG)
 
 class RA(Base, Queryable):
     # view in a schema
     __tablename__ = 'ramsar'
     __table_args__ = ({'schema': 'schutzge', 'autoload': False})
     __template__ = 'tooltips/ra.mako'
-
     id = Column('ra_id', Integer, primary_key=True)
     ra_id = column_property(id)
     ra_name = Column('ra_name', Text)
@@ -197,85 +214,94 @@ class RA(Base, Queryable):
     ra_gf = Column('ra_gf', Text)
     the_geom = Column(Geometry)
 	
+register('ch.bafu.schutzgebiete-ramsar', RA)
+
 class WILDRUHEZONEN(Base, Queryable):
     # view in a schema
     __tablename__ = 'wildruhezonen'
     __table_args__ = ({'schema': 'schutzge', 'autoload': True})
     __template__ = 'tooltips/wildruhezonen.mako'
-
     id = Column('wrz_obj', Integer, primary_key=True)
     the_geom = Column(Geometry)
     
+register('ch.bafu.schutzgebiete-wildruhezonen', WILDRUHEZONEN)
+
 class WILDRUHEZONENJAGDBANNGEBIETE(Base, Queryable):
     # view in a schema
     __tablename__ = 'wildruhezonen_jagdbanngebiete'
     __table_args__ = ({'schema': 'schutzge', 'autoload': True})
     __template__ = 'tooltips/wildruhezonen_jagdbanngebiete.mako'
-
     id = Column('wrz_jb_obj', Integer, primary_key=True)
     the_geom = Column(Geometry)
     
+register('ch.bafu.wildruhezonen-jagdbanngebiete', WILDRUHEZONENJAGDBANNGEBIETE)
+
 class WEGEWILDRUHEZONENJAGDBANNGEBIETE(Base, Queryable):
     # view in a schema
     __tablename__ = 'wege_wildruhezonen_jagdbanngebiete'
     __table_args__ = ({'schema': 'schutzge', 'autoload': True})
     __template__ = 'tooltips/wege_wildruhezonen_jagdbanngebiete.mako'
-
     id = Column('weg_id', Integer, primary_key=True)
     the_geom = Column(Geometry)
+
+register('ch.bafu.wege-wildruhezonen-jagdbanngebiete', WEGEWILDRUHEZONENJAGDBANNGEBIETE)
 
 class SB(Base, Queryable):
     # view in a schema
     __tablename__ = 'sb'
     __table_args__ = ({'schema': 'fauna', 'autoload': True})
     __template__ = 'tooltips/sb.mako'
-
     id = Column('gid', Integer, primary_key=True)
     the_geom = Column(Geometry)
+
+register('ch.bafu.fauna-steinbockkolonien', SB)
 
 class SWISSPRTR(Base, Queryable):
     # view in a schema
     __tablename__ = 'swissprtr'
     __table_args__ = ({'schema': 'prtr', 'autoload': True})
     __template__ = 'tooltips/swissprtr.mako'
-
     id = Column('prtrnr', Integer, primary_key=True)
     the_geom = Column(Geometry)
+
+register('ch.bafu.swissprtr', SWISSPRTR)
 
 class HOLZVORRAT(Base, Queryable):
     # view in a schema
     __tablename__ = 'holzvorrat'
     __table_args__ = ({'schema': 'wald', 'autoload': True})
     __template__ = 'tooltips/holzvorrat.mako'
-
     id = Column('gid', Integer, primary_key=True)
     fid = Column('id', Integer)
     the_geom = Column(Geometry)
+
+register('ch.bafu.holzvorrat', HOLZVORRAT)
 
 class HOLZZUWACHS(Base, Queryable):
     # view in a schema
     __tablename__ = 'holzzuwachs'
     __table_args__ = ({'schema': 'wald', 'autoload': True})
     __template__ = 'tooltips/holzzuwachs.mako'
-
     id = Column('gid', Integer, primary_key=True)
     the_geom = Column(Geometry)
+
+register('ch.bafu.holzzuwachs', HOLZZUWACHS)
 
 class HOLZNUTZUNG(Base, Queryable):
     # view in a schema
     __tablename__ = 'holznutzung'
     __table_args__ = ({'schema': 'wald', 'autoload': True})
     __template__ = 'tooltips/holznutzung.mako'
-
     id = Column('gid', Integer, primary_key=True)
     the_geom = Column(Geometry)
+
+register('ch.bafu.holznutzung', HOLZNUTZUNG)
 
 class NABEL(Base, Queryable):
     # view in a schema
     __tablename__ = 'nabel'
     __table_args__ = ({'schema': 'luft', 'autoload': False})
     __template__ = 'tooltips/nabel.mako'
-
     id = Column('id_stat', Text, primary_key=True)
     id_stat = column_property(id)
     name = Column('name', Text)
@@ -283,9 +309,9 @@ class NABEL(Base, Queryable):
     typ_fr = Column('typ_fr', Text)
     desc_de = Column('desc_de', Text)
     desc_fr = Column('desc_fr', Text)
-
     the_geom = Column(Geometry)
 
+register('ch.bafu.nabelstationen', NABEL)
 
 class krebspest(Base, Queryable):
     # view in a schema
@@ -319,7 +345,6 @@ class biosphaerenreservate(Base, Queryable):
     __tablename__ = 'biores'
     __table_args__ = ({'schema': 'schutzge', 'autoload': False})
     __template__ = 'tooltips/biosphaerenreservate.mako'
-
     id = Column('bgdi_id', Integer, primary_key=True)
     biores_ver = Column('biores_ver', Text)
     biores_fl = Column('biores_fl', Text)
@@ -460,31 +485,3 @@ class wrzportal(Base, Queryable):
 register('ch.bafu.wrz-wildruhezonen_portal', wrzportal)
 
 
-register('ch.bafu.bundesinventare-amphibien_wanderobjekte', AM_G)
-register('ch.bafu.bundesinventare-amphibien', AM_L)
-register('ch.bafu.hydrologie-hydromessstationen', LHG)
-register('ch.bafu.bundesinventare-auen', AU)
-register('ch.bafu.bundesinventare-bln', BLN)
-register('ch.bafu.bundesinventare-hochmoore', HM)
-register('ch.bafu.bundesinventare-jagdbanngebiete', JB)
-register('ch.bafu.bundesinventare-moorlandschaften', ML)
-register('ch.bafu.bundesinventare-vogelreservate', WV)
-register('ch.bafu.wasser-entnahme', WEWB)
-register('ch.bafu.wasser-entnahme', WEWW)
-register('ch.bafu.wasser-entnahme', WEANB)
-register('ch.bafu.wasser-entnahme', WEANW)
-register('ch.bafu.wasser-leitungen', WL)
-register('ch.bafu.wasser-rueckgabe', WR)
-register('ch.bafu.bundesinventare-flachmoore', FM)
-register('ch.bafu.bundesinventare-flachmoore', FM_REG)
-register('ch.bafu.schutzgebiete-paerke_nationaler_bedeutung', PAERKE_NATIONALER_BEDEUTUNG)
-register('ch.bafu.schutzgebiete-ramsar', RA)
-register('ch.bafu.schutzgebiete-wildruhezonen', WILDRUHEZONEN)
-register('ch.bafu.wildruhezonen-jagdbanngebiete', WILDRUHEZONENJAGDBANNGEBIETE)
-register('ch.bafu.wege-wildruhezonen-jagdbanngebiete', WEGEWILDRUHEZONENJAGDBANNGEBIETE)
-register('ch.bafu.fauna-steinbockkolonien', SB)
-register('ch.bafu.swissprtr', SWISSPRTR)
-register('ch.bafu.holzvorrat', HOLZVORRAT)
-register('ch.bafu.holzzuwachs', HOLZZUWACHS)
-register('ch.bafu.holznutzung', HOLZNUTZUNG)
-register('ch.bafu.nabelstationen', NABEL)
